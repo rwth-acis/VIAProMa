391a55645e431365dd8728fbe2a7a0814083d7b2
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-04-13 22:16:29 +0200

    started on Extendend Wilkinson implementation

---
+++ Frontend/VIAProMa/Assets/Scripts/ExtendedWilkinson.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendedWilkinson
+{
+    List<float> Q = new List<float>() { 1, 5, 2, 2.5f, 4, 3 };
+
+    public float SimplicityMax(float q, int j)
+    {
+        int n = Q.Count;
+        int i = Q.FindIndex((x) => { return x == q; });
+
+        if (i != -1)
+        {
+            i += 1; // correct to array numbering in paper (starting at 1 and not 0)
+        }
+        else
+        {
+            Debug.LogError("Could not find q=" + q + " in list Q");
+            return float.MinValue; // worst result
+        }
+
+        float res = 1 - (i - 1) / (n - 1) - j + 1;
+
+        return res;
+    }
+
+    /// <summary>
+    /// Computes the density measure for a number of labels
+    /// </summary>
+    /// <param name="k">Looped number of axis labels</param>
+    /// <param name="m">Number of axis labels</param>
+    /// <returns>The approximation for the density measure</returns>
+    public float DensityMax(int k, int m)
+    {
+        if (k >= m)
+        {
+            return 2f - (k - 1f) / (m - 1f);
+        }
+        else
+        {
+            return 1f;
+        }
+    }
+}
--- Frontend/VIAProMa/Assets/Scripts/Tester.cs
+++ Frontend/VIAProMa/Assets/Scripts/Tester.cs
@@ -10,38 +10,43 @@ public class Tester : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Bounds b = new Bounds();
-            //b.Encapsulate(new Vector3(0, 1, 0));
-            //b.Encapsulate(new Vector3(0, -1, 0));
+            ExtendedWilkinson w = new ExtendedWilkinson();
 
-            //Debug.Log(b);
-
-            Debug.Log("Test routine started");
-            ApiResult<PunchCardEntry[]> res = await BackendConnector.GetGitHubPunchCard("rwth-acis", "GaMR");
-            if (res.HasError)
-            {
-                return;
-            }
-
-            List<DataPoint> points = new List<DataPoint>();
-
-            Color[] colors = { Color.black, Color.blue, Color.cyan, Color.green, Color.magenta, Color.white, Color.yellow };
-
-            foreach (PunchCardEntry entry in res.Value)
-            {
-                Color color = colors[entry.day];
-                points.Add(new DataPoint(new Vector3(entry.day, entry.numberOfCommits, entry.hour), color));
-            }
+            w.SimplicityMax(2.5f, 2);
 
 
-            //List<Vector3> points = new List<Vector3>();
-            //points.Add(new Vector3(0, 0, 0));
-            //points.Add(new Vector3(0, 1, 0));
-            //points.Add(new Vector3(1, 1, 1));
-            //points.Add(new Vector3(0, 0, 1));
-            //points.Add(new Vector3(1, 0, 0));
+            ////Bounds b = new Bounds();
+            ////b.Encapsulate(new Vector3(0, 1, 0));
+            ////b.Encapsulate(new Vector3(0, -1, 0));
 
-            scatterplot.Points = points;
+            ////Debug.Log(b);
+
+            //Debug.Log("Test routine started");
+            //ApiResult<PunchCardEntry[]> res = await BackendConnector.GetGitHubPunchCard("rwth-acis", "GaMR");
+            //if (res.HasError)
+            //{
+            //    return;
+            //}
+
+            //List<DataPoint> points = new List<DataPoint>();
+
+            //Color[] colors = { Color.black, Color.blue, Color.cyan, Color.green, Color.magenta, Color.white, Color.yellow };
+
+            //foreach (PunchCardEntry entry in res.Value)
+            //{
+            //    Color color = colors[entry.day];
+            //    points.Add(new DataPoint(new Vector3(entry.day, entry.numberOfCommits, entry.hour), color));
+            //}
+
+
+            ////List<Vector3> points = new List<Vector3>();
+            ////points.Add(new Vector3(0, 0, 0));
+            ////points.Add(new Vector3(0, 1, 0));
+            ////points.Add(new Vector3(1, 1, 1));
+            ////points.Add(new Vector3(0, 0, 1));
+            ////points.Add(new Vector3(1, 0, 0));
+
+            //scatterplot.Points = points;
         }
     }
 }
