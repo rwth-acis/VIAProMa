c359f18b949e1219540313ff8e56ff27f8294bcf
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-04-28 22:25:55 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/WebConnection/ConnectionManager.cs
+++ Frontend/VIAProMa/Assets/Scripts/WebConnection/ConnectionManager.cs
@@ -3,12 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// provides the necessary data for a connection to the backend
+/// </summary>
 public class ConnectionManager : Singleton<ConnectionManager>
 {
+    [Tooltip("The address of the backend, i.e. its url and port")]
     [SerializeField] private string backendAddress = "http://localhost:8080";
 
+    [Tooltip("The base path of the backend's API.")]
     [SerializeField] private string basePath = "resources";
 
+    /// <summary>
+    /// The address of the backend, i.e. its url and port
+    /// </summary>
     public string BackendAddress
     {
         get
@@ -17,6 +25,9 @@ public class ConnectionManager : Singleton<ConnectionManager>
         }
     }
 
+    /// <summary>
+    /// Combination of hte backend address and the base path of the backend's API
+    /// </summary>
     public string BackendAPIBaseURL
     {
         get
