1fa5efa3aa0b03ac7a3dbcf5f86891cd3453020d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-05-05 01:34:43 +0200

    added script which arranges objects in an array, rotation on focus

---
+++ Frontend/VIAProMa/Assets/Scripts/ObjectArray.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectArray : MonoBehaviour
+{
+    [SerializeField] GameObject[] collection;
+
+    public float gap;
+
+    private BoxCollider[] colliders;
+
+    private void Awake()
+    {
+        colliders = new BoxCollider[collection.Length];
+        for (int i=0;i<collection.Length;i++)
+        {
+            collection[i].transform.parent = transform;
+            colliders[i] = collection[i].GetComponent<BoxCollider>();
+        }
+    }
+
+    private void Update()
+    {
+        // store the rotation and reset it to identity so that we do not need to worry about it
+        // in the end we will set the rotation back to what it was
+        Quaternion originalRot = transform.rotation;
+        transform.rotation = Quaternion.identity;
+        float x = 0;
+        for (int i = 0; i < collection.Length; i++)
+        {
+            x += colliders[i].bounds.size.x / 2f;
+            collection[i].transform.localPosition = new Vector3(x, 0, 0);
+            x += colliders[i].bounds.size.x / 2f + gap;
+        }
+        float fullWidth = x - gap; // x has now accumulated the full width of the array + one gap
+        // now move the objects back by half the full width so that they are centered on the parent
+        for (int i=0;i<collection.Length;i++)
+        {
+            collection[i].transform.localPosition -= new Vector3(fullWidth / 2f, 0, 0);
+        }
+        transform.rotation = originalRot;
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/RotateOnFocus.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnFocus : BaseFocusHandler
+{
+    [SerializeField] Vector3 standardEulerRotation;
+    [SerializeField] Vector3 focusedEulerRotation;
+
+    public float damping = 10;
+
+    Vector3 targetRotation;
+
+    public override void OnFocusEnter(FocusEventData eventData)
+    {
+        base.OnFocusEnter(eventData);
+        targetRotation = focusedEulerRotation;
+    }
+
+    public override void OnFocusExit(FocusEventData eventData)
+    {
+        base.OnFocusExit(eventData);
+        targetRotation = standardEulerRotation;
+    }
+
+    private void Awake()
+    {
+        transform.localEulerAngles = standardEulerRotation;
+        targetRotation = standardEulerRotation;
+    }
+
+    private void Update()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRotation), Time.deltaTime * damping);
+    }
+}
