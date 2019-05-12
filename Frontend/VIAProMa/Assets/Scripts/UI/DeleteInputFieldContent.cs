5d322f5ae77bbbe64519e69665b0a06726f94ea8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-05-12 21:00:59 +0200

    added input field UI

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/DeleteInputFieldContent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputField))]
+public class DeleteInputFieldContent : MonoBehaviour
+{
+    private InputField inputField;
+
+    public void DeleteContent()
+    {
+        inputField.Text = "";
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/UI/InputField.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputField : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro contentField;
+
+    [SerializeField] private string text = "Content";
+
+    public string Text
+    {
+        get { return text; }
+        set
+        {
+            text = value;
+            ApplyTextToDisplay();
+        }
+    }
+
+    private void Awake()
+    {
+        if (contentField == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(contentField));
+        }
+    }
+
+    private void ApplyTextToDisplay()
+    {
+        if (contentField != null)
+        {
+            contentField.text = text;
+        }
+    }
+
+    private void OnValidate()
+    {
+        ApplyTextToDisplay();
+    }
+}
