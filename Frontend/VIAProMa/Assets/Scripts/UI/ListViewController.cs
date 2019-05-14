69bbca3aed7825630e2cb59c568749649d637ee3
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-05-14 20:20:00 +0200

    started on generic 3D lists

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/ListViewController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListViewController<DataType, ItemType> : MonoBehaviour
+    where DataType : IListViewItemData
+    where ItemType : ListViewItem<DataType>
+{
+    [SerializeField] protected GameObject itemPrefab;
+
+    public Vector3 offset;
+
+    public List<DataType> Data { get; protected set; }
+
+    private void Awake()
+    {
+        Data = new List<DataType>();
+        if (itemPrefab == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(itemPrefab));
+        }
+    }
+
+    protected virtual void CreateInstances()
+    {
+
+    }
+}
+
+public class ListViewController : ListViewController<ListViewItemInspectorData, ListViewItem>
+{
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/UI/ListViewItemStringData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListViewStringItemData : IListViewItemData
+{
+    public string text;
+}
