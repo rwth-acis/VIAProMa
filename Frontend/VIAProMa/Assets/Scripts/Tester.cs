0be136d96b6ef3d64fe4762d48c40d13da91ee8e
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-05-19 22:15:34 +0200

    re-enabled testing script

--- Frontend/VIAProMa/Assets/Scripts/Tester.cs
+++ Frontend/VIAProMa/Assets/Scripts/Tester.cs
@@ -14,29 +14,29 @@ public class Tester : MonoBehaviour
             Debug.Log("Test routine started");
 
 
-            //ApiResult<PunchCardEntry[]> res = await BackendConnector.GetGitHubPunchCard("rwth-acis", "GaMR");
-            //if (res.HasError)
-            //{
-            //    return;
-            //}
-            //List<DataPoint> points = new List<DataPoint>();
+            ApiResult<PunchCardEntry[]> res = await BackendConnector.GetGitHubPunchCard("rwth-acis", "GaMR");
+            if (res.HasError)
+            {
+                return;
+            }
+            List<DataPoint> points = new List<DataPoint>();
 
-            //Color[] colors = { Color.black, Color.blue, Color.cyan, Color.green, Color.magenta, Color.white, Color.yellow };
+            Color[] colors = { Color.black, Color.blue, Color.cyan, Color.green, Color.magenta, Color.white, Color.yellow };
 
-            //foreach (PunchCardEntry entry in res.Value)
-            //{
-            //    Color color = colors[entry.day];
-            //    points.Add(new DataPoint(new Vector3(entry.day, entry.numberOfCommits, entry.hour), color));
-            //}
+            foreach (PunchCardEntry entry in res.Value)
+            {
+                Color color = colors[entry.day];
+                points.Add(new DataPoint(new Vector3(entry.day, entry.numberOfCommits, entry.hour), color));
+            }
 
-            //Axis xAxis = new Axis() { Labels = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, Title = "Weekday" };
-            //Axis yAxis = new Axis() { Title = "Number of Commits" };
-            //Axis zAxis = new Axis() { Title = "Hour" };
+            Axis xAxis = new Axis() { Labels = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, Title = "Weekday" };
+            Axis yAxis = new Axis() { Title = "Number of Commits" };
+            Axis zAxis = new Axis() { Title = "Hour" };
 
-            //DataSet set = new DataSet() { Points = points, XAxis = xAxis, YAxis = yAxis, ZAxis = zAxis };
+            DataSet set = new DataSet() { Points = points, XAxis = xAxis, YAxis = yAxis, ZAxis = zAxis };
 
-            //barChart.BoxSize = boxSize;
-            //barChart.DataSet = set;
+            barChart.BoxSize = boxSize;
+            barChart.DataSet = set;
         }
     }
 }
