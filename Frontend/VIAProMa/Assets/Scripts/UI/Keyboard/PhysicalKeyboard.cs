1babcf4b7915751bd21afb7c2da3afd43ca41ddb
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-05-28 12:51:48 +0200

    added support for physical keyboard

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/Keyboard/PhysicalKeyboard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalKeyboard : MonoBehaviour
+{
+    public bool IsCapturingKeyboard { get; set; }
+
+    private Keyboard keyboard;
+
+    private void Awake()
+    {
+        keyboard = GetComponent<Keyboard>();
+        IsCapturingKeyboard = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsCapturingKeyboard)
+        {
+            foreach(char c in Input.inputString)
+            {
+                if (c == '\b') // backspace
+                {
+                    keyboard.Backspace();
+                }
+                else
+                {
+                    keyboard.AddLetter(c);
+                }
+            }
+        }
+    }
+}
