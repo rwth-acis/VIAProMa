37412a89a12e1c0b9373a7bda66c1f0f2085105e
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-06-18 19:18:19 +0200

    implemented foot menu rotation control

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/FootMenuMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootMenuMover : MonoBehaviour
+{
+    public float maxHeight = 1.5f;
+
+    private void Update()
+    {
+        float heightRatio = 1f - Mathf.Clamp01(transform.parent.position.y / maxHeight);
+        float tiltAngle = heightRatio * 90f;
+
+        transform.localEulerAngles = new Vector3(
+            tiltAngle,
+            transform.localEulerAngles.y,
+            0f
+            );
+    }
+}
