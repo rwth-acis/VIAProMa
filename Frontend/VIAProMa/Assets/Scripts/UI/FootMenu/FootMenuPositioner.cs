94f5c38f1e6dc838c38abdce8ae68b0031508799
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-06-19 15:32:20 +0200

    added positioning script for foot menu

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/FootMenu/FootMenuPositioner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootMenuPositioner : MonoBehaviour
+{
+    public float height = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Camera.main.transform.position.x,
+            height,
+            Camera.main.transform.position.z
+            );
+    }
+}
