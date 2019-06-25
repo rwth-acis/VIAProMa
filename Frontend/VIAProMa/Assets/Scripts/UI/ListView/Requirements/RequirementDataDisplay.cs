db125a20670ff4012f5d9b0785d2188349442f5d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-06-25 21:24:51 +0200

    added data display to requirements card

--- Frontend/VIAProMa/Assets/Scripts/UI/ListView/Requirements/RequirementDataDisplay.cs
+++ Frontend/VIAProMa/Assets/Scripts/UI/ListView/Requirements/RequirementDataDisplay.cs
@@ -7,11 +7,13 @@ public class RequirementDataDisplay : DataDisplay<Requirement>
 {
     [SerializeField] private TextMeshPro titleField;
     [SerializeField] private TextMeshPro descriptionField;
+    [SerializeField] private TextMeshPro creatorField;
 
     public override void UpdateView()
     {
         base.UpdateView();
         titleField.text = content.name;
         descriptionField.text = content.description;
+        creatorField.text = content.creator.userName;
     }
 }
