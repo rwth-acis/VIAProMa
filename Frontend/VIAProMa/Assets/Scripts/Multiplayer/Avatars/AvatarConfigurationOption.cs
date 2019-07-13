7bc62398fa9bd5ab615ad7a26d3c8aba2ce9aa6d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-13 13:04:04 +0200

    rigged and weight painted first avatar

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/AvatarConfigurationOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class AvatarConfigurationOption
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    public Mesh Mesh { get => mesh; }
+    public Material Material { get => material; }
+}
