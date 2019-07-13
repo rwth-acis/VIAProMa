4cca08cfc0246ef52468053c5887cb4d91da0200
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-13 18:53:31 +0200

    implemented avatar eye movement

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/EyeTargetFollower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeTargetFollower : MonoBehaviour
+{
+    private Transform headBone;
+
+    public Transform eyeTarget;
+    public float minHorizontalRotation;
+    public float maxHorizontalRotation;
+    public float minVerticalRotation;
+    public float maxVerticalRotation;
+
+    private void Awake()
+    {
+        if (eyeTarget == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(eyeTarget));
+        }
+        headBone = transform.parent;
+    }
+
+    void Update()
+    {
+        Vector3 eyeToTarget = eyeTarget.position - transform.position;
+        Vector3 projectedForward = new Vector3(0, 0, -1);
+
+        Vector3 horizontalEyeToTarget = Vector3.ProjectOnPlane(eyeToTarget, headBone.up);
+        float horizontalAngle = Vector3.SignedAngle(projectedForward, horizontalEyeToTarget, headBone.up);
+
+        Vector3 verticalEyeToTarget = Vector3.ProjectOnPlane(eyeToTarget, headBone.right);
+        float verticalAngle = Vector3.SignedAngle(projectedForward, verticalEyeToTarget, headBone.right);
+
+        transform.localEulerAngles = new Vector3(
+            Mathf.Clamp(verticalAngle, minVerticalRotation, maxVerticalRotation) - 90f, // correction because the bone starts at -90
+            Mathf.Clamp(horizontalAngle, minHorizontalRotation, maxHorizontalRotation),
+            0f
+            );
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/Utilities/FollowObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = target.position;
+        }
+    }
+}
