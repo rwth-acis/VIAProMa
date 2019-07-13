dede7944740b4008cb2594b71cbd5f54d6e07619
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-13 22:02:14 +0200

    prototyped customization options

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/CustomizationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class CustomizationController : MonoBehaviour
+{
+    public AvatarConfigurationOption[] options;
+
+    private SkinnedMeshRenderer skinnedRenderer;
+
+    int index = 0;
+
+    private void Awake()
+    {
+        skinnedRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            skinnedRenderer.sharedMesh = options[index].Mesh;
+            skinnedRenderer.material = options[index].Material;
+            index++;
+            index %= options.Length;
+        }
+    }
+}
