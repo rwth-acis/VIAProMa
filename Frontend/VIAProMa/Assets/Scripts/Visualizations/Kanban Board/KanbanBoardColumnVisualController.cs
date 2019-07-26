ee2193fa19fe200f261bee7f19ec1b592f725711
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-26 15:54:24 +0200

    implemented Kanban board size control

--- Frontend/VIAProMa/Assets/Scripts/Visualizations/Kanban Board/KanbanBoardColumnVisualController.cs
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/Kanban Board/KanbanBoardColumnVisualController.cs
@@ -14,17 +14,27 @@ public class KanbanBoardColumnVisualController : MonoBehaviour
 
 
     private List<Issue> issues;
-    private float issueCardHeight;
+    private Vector2 issueCardSize;
+
+    private Vector2 size;
+
+    public float Width
+    {
+        get => background.localScale.y;
+        set
+        {
+            size.x = Mathf.Max(issueCardSize.x, value); // do not allow values which are smaller than the issueCard
+            UpdateSize();
+        }
+    }
 
     public float Height
     {
         get => background.localScale.y;
         set
         {
-            if (value >= headerBackground.localScale.y) // do not allow negative values or 0
-            {
-                SetHeight(value);
-            }
+            size.y = Mathf.Max(headerBackground.localScale.y, value); // do not allow negative values or 0
+            UpdateSize();
         }
     }
 
@@ -70,6 +80,34 @@ public class KanbanBoardColumnVisualController : MonoBehaviour
                 SpecialDebugMessages.LogComponentNotFoundError(this, nameof(BoxCollider), issueListView.ItemPrefab);
             }
         }
+
+        size = new Vector2(background.localScale.x, background.localScale.y);
+        UpdateSize();
+    }
+
+    private void UpdateSize()
+    {
+        // the background has the whole size
+        background.localScale = new Vector3(
+            size.x,
+            size.y,
+            background.localScale.z
+            );
+
+        // the header should be at the top and adapts to the width
+        headerBackground.localScale = new Vector3(
+            size.x,
+            headerBackground.localScale.y,
+            headerBackground.localScale.z
+            );
+        header.localPosition = new Vector3(
+            0,
+            size.y / 2f - headerBackground.localScale.y / 2f, // position at top
+            0);
+        headerTitle.rectTransform.sizeDelta = new Vector2(size.x, headerBackground.localScale.y);
+
+        // the list view should be underneath the header
+        issueListView.transform.localPosition = header.localPosition - new Vector3(0, header.localScale.y / 2f + issueCardSize.y / 2f, 0);
     }
 
     private void SetHeight(float height)
--- Frontend/VIAProMa/Assets/Tests/Visualizations/KanbanBoardTest/KanbanBoardColumnTestRunner.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/KanbanBoardTest/KanbanBoardColumnTestRunner.cs
@@ -6,12 +6,14 @@ public class KanbanBoardColumnTestRunner : MonoBehaviour
 {
     public KanbanBoardColumnVisualController visualController;
 
+    public float width = 0.5f;
     public float height = 1f;
 
     private void Update()
     {
         if (visualController != null)
         {
+            visualController.Width = width;
             visualController.Height = height;
         }
     }
