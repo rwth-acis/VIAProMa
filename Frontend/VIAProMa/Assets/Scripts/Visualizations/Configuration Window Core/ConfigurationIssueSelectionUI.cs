9c53388adfaddaa53cedec7391114923cc51b120
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-27 21:26:16 +0200

    refactored configuration window issue selection

--- Frontend/VIAProMa/Assets/Scripts/Visualizations/Configuration Window Core/ConfigurationIssueSelectionUI.cs
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/Configuration Window Core/ConfigurationIssueSelectionUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConfigurationIssueSelectionUI : MonoBehaviour
+public class ConfigurationIssueSelectionUI : MonoBehaviour, IUiFragment
 {
     [SerializeField] private Interactable selectionButton;
     [SerializeField] private GameObject selectionActiveMessage;
---
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/Configuration Window Core/IUiFragment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUiFragment
+{
+    bool UIEnabled { get; set; }
+
+    void Setup(Visualization visualization);
+}
