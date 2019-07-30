e7274e775304f4abe66e4a23e999d5ce730f8bde
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-07-31 00:26:19 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/Visualizations/IColorChangeable.cs
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/IColorChangeable.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Interface for objects where the color can be changed
+/// </summary>
 public interface IColorChangeable
 {
+    /// <summary>
+    /// The main color which can be changed on the accompanying GameObject
+    /// </summary>
     Color Color { get; set; }
 }
--- Frontend/VIAProMa/Assets/Scripts/Visualizations/IVisualizationContentProvider.cs
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/IVisualizationContentProvider.cs
@@ -3,12 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A visualization content provider can be used by a visualization to determine which issues should be used in the visualization
+/// </summary>
 public interface IVisualizationContentProvider
 {
+    /// <summary>
+    /// The list of issues which define the content of the visualization
+    /// </summary>
     List<Issue> Issues { get; }
 
+    /// <summary>
+    /// Event which is called if the issue list is changed
+    /// </summary>
     event EventHandler ContentChanged;
 
+    /// <summary>
+    /// Starts the selection process in order to define the issues which belong to this content provider
+    /// </summary>
     void SelectContent();
+
+    /// <summary>
+    /// Ends the selection mode and assigns the selected issues as content
+    /// </summary>
     void EndContentSelection();
 }
