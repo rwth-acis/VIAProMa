b90a359a5d3e82b0baf9a072419b720c7245cba7
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-05 12:23:33 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/AvatarConfigurationOption.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/AvatarConfigurationOption.cs
@@ -10,7 +10,16 @@ public class AvatarConfigurationOption
     [SerializeField] private Mesh mesh;
     [SerializeField] private MaterialConfigurationOption[] materialVariants;
 
+    /// <summary>
+    /// Name of the avatar configuration option
+    /// </summary>
     public string Name { get => name; }
+    /// <summary>
+    /// Mesh which represents the avatar configuration option
+    /// </summary>
     public Mesh Mesh { get => mesh; }
+    /// <summary>
+    /// Array of material variants which the configuration option offers
+    /// </summary>
     public MaterialConfigurationOption[] MaterialVariants { get => materialVariants; }
 }
