b157aceb5014dfdf90914df63d0eb94553b2ea7b
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-14 23:06:07 +0200

    added avatar config gizmo icons, set shirt color variations

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/AvatarSpineController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSpineController : MonoBehaviour
+{
+    [SerializeField] private Transform chestBone;
+    [SerializeField] private Transform neckBone;
+    [SerializeField] private Transform headBone;
+
+    private Vector3 position;
+    private Vector3 eulerAngles;
+
+    private Vector3 initialChestRotation;
+    private Vector3 initialNeckRotation;
+    private Vector3 initialHeadRotation;
+}
