e87f7fc14fe2cec4fdec1d0d4cb95f16ceb0c388
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-16 21:44:19 +0200

    extended diagnostic system with different send modes

--- Frontend/VIAProMa/Assets/Scripts/Utilities/DiagnosticsSender.cs
+++ Frontend/VIAProMa/Assets/Scripts/Utilities/DiagnosticsSender.cs
@@ -3,33 +3,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Collects log outputs from Unity's console and sends them to the backend
+/// </summary>
 public class DiagnosticsSender : MonoBehaviour
 {
+    [Tooltip("Determines when logs are send.")]
+    public DiagnosticSendMode sendMode = DiagnosticSendMode.ON_ERROR_WARNING;
+
+    [Tooltip("If Send Mode is set to Interval, the value determines the interval in seconds when the logs should be sent.")]
     public int sendInterval = 10;
+
+    /// <summary>
+    /// Determines how many messages are at most stored and sent
+    /// If set to 0, the buffer size is unlimited.
+    /// Not used if sendMode is set to INTERVAL.
+    /// </summary>
+    [Tooltip("Determines how many messages are at most stored and sent. If set to 0, the buffer size is unlimited. Not used if Send Mode is Interval.")]
+    public int maxMessageBufferSize = 10;
 
     private List<string> logs;
 
+    /// <summary>
+    /// Initializes the component
+    /// </summary>
     private void Awake()
     {
         logs = new List<string>();
     }
 
+    /// <summary>
+    /// Initiates the sending routine if the DiagnosticSendMode is set to INTERVAL
+    /// </summary>
     private void Start()
     {
-        InvokeRepeating("SendLogs", 1f, sendInterval);
+        if (sendMode == DiagnosticSendMode.INTERVAL)
+        {
+            InvokeRepeating("SendLogs", 1f, sendInterval);
+        }
     }
 
+    /// <summary>
+    /// Registers to receive console logs
+    /// </summary>
     private void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
     }
 
+    /// <summary>
+    /// Un-registers from console logs
+    /// </summary>
     private void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
     }
 
+    /// <summary>
+    /// Called if a new log was written to console
+    /// Creates a log string and adds it to the internal log list
+    /// </summary>
+    /// <param name="logString">String which was written to console</param>
+    /// <param name="stackTrace">The stack trace of the message</param>
+    /// <param name="type">Type of the message</param>
     private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        string logLine = CreateLogLine(logString, stackTrace, type);
+        logs.Add(logLine);
+
+        if (sendMode != DiagnosticSendMode.INTERVAL && maxMessageBufferSize > 0)
+        {
+            while(maxMessageBufferSize > logs.Count)
+            {
+                logs.RemoveAt(0);
+            }
+        }
+
+        if ((type == LogType.Error || type == LogType.Exception) && (sendMode == DiagnosticSendMode.ON_ERROR || sendMode == DiagnosticSendMode.ON_ERROR_WARNING))
+        {
+            SendLogs();
+        }
+        else if (type == LogType.Warning && (sendMode == DiagnosticSendMode.ON_ERROR_WARNING))
+        {
+            SendLogs();
+        }
+    }
+
+    /// <summary>
+    /// Creates a string representation of a console message
+    /// </summary>
+    /// <param name="logString">String which was written to console</param>
+    /// <param name="stackTrace">The stack trace of the message</param>
+    /// <param name="type">The type of the message</param>
+    /// <returns>A string representation for sending</returns>
+    private static string CreateLogLine(string logString, string stackTrace, LogType type)
     {
         string logLine = "";
         if (type == LogType.Error || type == LogType.Exception)
@@ -40,9 +107,13 @@ public class DiagnosticsSender : MonoBehaviour
         logLine += logString;
         logLine += "\n" + stackTrace;
         logLine += "\n";
-        logs.Add(logLine);
+        return logLine;
     }
 
+    /// <summary>
+    /// Sends the collected logs to the backend.
+    /// Also clears the logs once they have been sent.
+    /// </summary>
     private void SendLogs()
     {
         if (logs.Count > 0)
@@ -56,4 +127,23 @@ public class DiagnosticsSender : MonoBehaviour
             logs.Clear();
         }
     }
+}
+
+/// <summary>
+/// The send mode for the diagnostic system
+/// </summary>
+public enum DiagnosticSendMode
+{
+    /// <summary>
+    /// Sending should only happen if an error was logged
+    /// </summary>
+    ON_ERROR,
+    /// <summary>
+    /// Sending should only happen if an error or warning was logged
+    /// </summary>
+    ON_ERROR_WARNING,
+    /// <summary>
+    /// Sending should happen frequently
+    /// </summary>
+    INTERVAL
 }
