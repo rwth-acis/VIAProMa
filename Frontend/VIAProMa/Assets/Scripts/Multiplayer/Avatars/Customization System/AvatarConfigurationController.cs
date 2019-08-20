a56c415bfb1dd9e58bcaedfc6baf0eea2f3a5245
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-21 00:37:34 +0200

    added avatar initialization

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/AvatarConfigurationController.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/AvatarConfigurationController.cs
@@ -7,4 +7,15 @@ public class AvatarConfigurationController : MonoBehaviour
     [SerializeField] private AvatarPartControllerCategory[] avatarPartControllers;
 
     public AvatarPartControllerCategory[] AvatarPartControllers { get => avatarPartControllers; }
+
+    /// <summary>
+    /// Initializes the avatar by apply the initial configuration
+    /// </summary>
+    private void Awake()
+    {
+        foreach(AvatarPartControllerCategory category in avatarPartControllers)
+        {
+            category.ConfigurationController.ApplyConfiguration();
+        }
+    }
 }
