d98ed2c7068161482963c2e793e46a6349fc3cff
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-21 00:14:55 +0200

    dynamic category tabs

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/Configurator/AvatarConfigurator.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/Configurator/AvatarConfigurator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,11 +8,13 @@ using UnityEngine;
 public class AvatarConfigurator : MonoBehaviour
 {
     [SerializeField] private GameObject avatarParent;
-    [SerializeField] private InteractableToggleCollection categoryCollection;
+    [SerializeField] private InteractableToggleCollection categoryToggles;
+    [SerializeField] private GameObject categoryButtonPrefab;
     [SerializeField] private VariantSelector modelSelector;
     [SerializeField] private VariantSelector materialSelector;
     [SerializeField] private VariantSelector colorSelector;
 
+    private GridObjectCollection categoryObjectCollection;
     private AvatarConfigurationController avatarConfigurationController;
     private AvatarSpineController spineController;
 
@@ -26,13 +29,55 @@ public class AvatarConfigurator : MonoBehaviour
             EnsureAvatarConfigController();
         }
 
-        if (categoryCollection == null)
+        if (categoryToggles == null)
         {
-            SpecialDebugMessages.LogMissingReferenceError(this, nameof(categoryCollection));
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(categoryToggles));
         }
+        else
+        {
+            categoryObjectCollection = categoryToggles.gameObject.GetComponent<GridObjectCollection>();
+            if (categoryObjectCollection == null)
+            {
+                SpecialDebugMessages.LogComponentNotFoundError(this, nameof(GridObjectCollection), gameObject);
+            }
+        }
+        if (categoryButtonPrefab == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(categoryButtonPrefab));
+        }
+        if (modelSelector == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(modelSelector));
+        }
+        if (materialSelector == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(materialSelector));
+        }
+        if (colorSelector == null)
+        {
+            SpecialDebugMessages.LogMissingReferenceError(this, nameof(colorSelector));
+        }
+
+        InitializeCategories();
 
         modelSelector.ItemSelected += ModelItemSelected;
         materialSelector.ItemSelected += MaterialItemSelected;
+    }
+
+    private void InitializeCategories()
+    {
+        List<Interactable> categoryInteractables = new List<Interactable>();
+        for (int i=0;i<avatarConfigurationController.AvatarPartControllers.Length;i++)
+        {
+            GameObject categoryBtnInstance = Instantiate(categoryButtonPrefab, categoryToggles.transform);
+            Interactable categoryBtn = categoryBtnInstance.GetComponent<Interactable>();
+            categoryBtn.OnClick.AddListener(OnCategorySelection);
+            SpriteRenderer iconRenderer = categoryBtnInstance.GetComponentInChildren<SpriteRenderer>();
+            iconRenderer.sprite = avatarConfigurationController.AvatarPartControllers[i].Icon;
+            categoryInteractables.Add(categoryBtn);
+        }
+        categoryObjectCollection.UpdateCollection();
+        categoryToggles.ToggleList = categoryInteractables.ToArray();
     }
 
     private void Start()
@@ -51,7 +96,7 @@ public class AvatarConfigurator : MonoBehaviour
     public void OnCategorySelection()
     {
         EnsureAvatarConfigController();
-        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryCollection.CurrentIndex].ConfigurationController;
+        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryToggles.CurrentIndex].ConfigurationController;
         modelSelector.Items = selectedPartController.AvatarParts;
         modelSelector.SelectedIndex = selectedPartController.ModelIndex;
 
@@ -62,7 +107,7 @@ public class AvatarConfigurator : MonoBehaviour
     private void ModelItemSelected(object sender, EventArgs e)
     {
         EnsureAvatarConfigController();
-        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryCollection.CurrentIndex].ConfigurationController;
+        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryToggles.CurrentIndex].ConfigurationController;
         selectedPartController.ModelIndex = modelSelector.SelectedIndex;
         selectedPartController.ApplyConfiguration();
         materialSelector.Items = selectedPartController.AvatarPartMaterials;
@@ -71,7 +116,7 @@ public class AvatarConfigurator : MonoBehaviour
     private void MaterialItemSelected(object sender, EventArgs e)
     {
         EnsureAvatarConfigController();
-        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryCollection.CurrentIndex].ConfigurationController;
+        AvatarPartConfigurationController selectedPartController = avatarConfigurationController.AvatarPartControllers[categoryToggles.CurrentIndex].ConfigurationController;
         selectedPartController.MaterialIndex = materialSelector.SelectedIndex;
         selectedPartController.ApplyConfiguration();
     }
