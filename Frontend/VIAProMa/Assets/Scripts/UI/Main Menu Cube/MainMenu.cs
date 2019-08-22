998fd38a53e1eb27a8a05fac66bcc3b8b24a0cd8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-22 22:13:18 +0200

    added functionality to main menu close button, started on window manager

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/Main Menu Cube/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FoldController))]
+public class MainMenu : MonoBehaviour
+{
+
+
+    private FoldController foldController;
+
+
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/UI/WindowManager.cs
@@ -0,0 +1,20 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : Singleton<WindowManager>
+{
+    [SerializeField] private RoomMenu roomMenu;
+    [SerializeField] private ServerStatusMenu serverStatusMenu;
+
+    public RoomMenu RoomMenu
+    {
+        get => roomMenu;
+    }
+
+    public ServerStatusMenu ServerStatusMenu
+    {
+        get => serverStatusMenu;
+    }
+}
