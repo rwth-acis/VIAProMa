1e1a25ac12f69c9286463a14fb72e5877b82dc59
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-26 13:42:44 +0200

    started on issue shelf synchronization

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Common/TransformSynchronizer.cs
@@ -0,0 +1,52 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSynchronizer : MonoBehaviourPun, IPunObservable
+{
+    protected Vector3 targetPosition;
+    protected Quaternion targetRotation;
+
+    public float lerpSpeed = 15;
+
+    public static void PutPositionRotation(Transform targetTransform, ref PhotonStream stream)
+    {
+        stream.SendNext(targetTransform.position);
+        stream.SendNext(targetTransform.rotation);
+    }
+
+    public static void GetPositionRotation(ref PhotonStream stream, out Vector3 position, out Quaternion rotation)
+    {
+        position = (Vector3)(stream.ReceiveNext());
+        rotation = (Quaternion)(stream.ReceiveNext());
+    }
+
+    public static void SmoothPosition(Transform targetTransform, Vector3 targetPosition, float lerpSpeed)
+    {
+        targetTransform.position = Vector3.Lerp(targetTransform.position, targetPosition, lerpSpeed * Time.deltaTime);
+    }
+
+    public static void SmoothRotation(Transform targetTransform, Quaternion targetRotation, float lerpSpeed)
+    {
+        targetTransform.rotation = Quaternion.Lerp(targetTransform.rotation, targetRotation, lerpSpeed * Time.deltaTime);
+    }
+
+    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            PutPositionRotation(transform, ref stream);
+        }
+        else
+        {
+            GetPositionRotation(ref stream, out targetPosition, out targetRotation);
+        }
+    }
+
+    protected virtual void Update()
+    {
+        SmoothPosition(transform, targetPosition, lerpSpeed);
+        SmoothRotation(transform, targetRotation, lerpSpeed);
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/Shelf/IssueShelf/IssueShelfSynchronizer.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IssueShelfSynchronizer : TransformSynchronizer
+{
+    [PunRPC]
+    private void SetActive(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+
+    private void OnEnable()
+    {
+        photonView.RPC("SetActive", RpcTarget.Others, true);
+    }
+
+    private void OnDisable()
+    {
+        photonView.RPC("SetActive", RpcTarget.Others, false);
+    }
+}
