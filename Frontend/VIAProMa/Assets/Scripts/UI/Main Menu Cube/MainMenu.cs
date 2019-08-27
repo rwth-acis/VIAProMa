de4fa6dbf4e74c7b4b125036f0688c2646a61383
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-27 23:10:54 +0200

    Revert "changed main menu initialization"
    This reverts commit 4bd3361b598394395a3817bd0699e46e5fbc230c.

--- Frontend/VIAProMa/Assets/Scripts/UI/Main Menu Cube/MainMenu.cs
+++ Frontend/VIAProMa/Assets/Scripts/UI/Main Menu Cube/MainMenu.cs
@@ -80,12 +80,6 @@ public class MainMenu : MonoBehaviour
         Launcher.Instance.ConnectionStatusChanged -= OnConnectionStatusChanged;
     }
 
-    private void Start()
-    {
-        issueShelf.SetActive(false);
-        visualizationShelf.SetActive(false);
-    }
-
     private void OnConnectionStatusChanged(object sender, EventArgs e)
     {
         roomButton.Enabled = PhotonNetwork.IsConnected;
