fd562eba3ce4ec24e81c342547ee8593fbdf62a4
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-28 19:27:01 +0200

    fixed nullreference exception on special debug messages

--- Frontend/VIAProMa/Assets/Scripts/Utilities/SpecialDebugMessages.cs
+++ Frontend/VIAProMa/Assets/Scripts/Utilities/SpecialDebugMessages.cs
@@ -11,7 +11,7 @@ public static class SpecialDebugMessages
 
     public static void LogComponentNotFoundError(MonoBehaviour thisComponent, string searchedComponent, GameObject target)
     {
-        Debug.LogError("Error: Component of type " + searchedComponent + " not found\n" + thisComponent.GetType().Name + " looked for the component " + searchedComponent + " on the GameObject " + target.name + " but could not find it. Maybe you need to add it to " + target.name + "?", thisComponent);
+        Debug.LogError("Error: Component of type " + searchedComponent + " not found\n" + thisComponent.GetType().Name + " looked for the component " + searchedComponent + " on the GameObject " + target?.name + " but could not find it. Maybe you need to add it to " + target?.name + "?", thisComponent);
     }
 
     public static void LogArrayInitializedWithSize0Warning(MonoBehaviour thisComponent, string arrayName)
