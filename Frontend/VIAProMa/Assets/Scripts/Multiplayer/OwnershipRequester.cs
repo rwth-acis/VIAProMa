b4cb32df4fb2044c5e5a6507d6b9274a3e1bebed
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-01 00:11:42 +0200

    added logo

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipRequester.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipRequester.cs
@@ -7,15 +7,24 @@ public class OwnershipRequester : MonoBehaviourPun
 {
     public void OnManipulationStarted()
     {
-        if (photonView.Owner == PhotonNetwork.LocalPlayer)
+        // since we are dealing with scene objects, there is no owner in the beginning
+        if (photonView.Owner != null)
         {
-            Debug.Log("Locking object", gameObject);
-            OwnershipManager.Instance.DisableOwnerShipTransfer(photonView);
+            Debug.Log(photonView.Owner + " vs. " + PhotonNetwork.LocalPlayer);
+            if (photonView.Owner == PhotonNetwork.LocalPlayer)
+            {
+                Debug.Log("Locking object", gameObject);
+                OwnershipManager.Instance.DisableOwnerShipTransfer(photonView);
+            }
+            else
+            {
+                Debug.Log("Requesting ownership", gameObject);
+                photonView.RequestOwnership();
+            }
         }
         else
         {
-            Debug.Log("Requesting ownership", gameObject);
-            photonView.RequestOwnership();
+            photonView.TransferOwnership(PhotonNetwork.LocalPlayer.ActorNumber);
         }
     }
 
