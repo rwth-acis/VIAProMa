a91a0a699335b553092ece1762d5702e64394873
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-08-31 20:19:51 +0200

    implemented networked ownership transfer

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipManager.cs
@@ -0,0 +1,48 @@
+﻿using HoloToolkit.Unity;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnershipManager : Singleton<OwnershipManager>
+{
+    private List<PhotonView> lockedViews;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        lockedViews = new List<PhotonView>();
+    }
+
+    public void DisableOwnerShipTransfer(PhotonView view)
+    {
+        // do not allow duplicates and make sure that only the owner can lock objects
+        if (!lockedViews.Contains(view) && view.Owner == PhotonNetwork.LocalPlayer)
+        {
+            lockedViews.Add(view);
+        }
+    }
+
+    public void EnableOwnerShipTransfer(PhotonView view)
+    {
+        // make sure that only the owner can unlock objects
+        if (view.Owner == PhotonNetwork.LocalPlayer)
+        {
+            lockedViews.Remove(view);
+        }
+    }
+
+    public void OnOwnershipRequest(object[] viewAndPlayer)
+    {
+        PhotonView view = (PhotonView)viewAndPlayer[0];
+        Player requestingPlayer = (Player)viewAndPlayer[1];
+
+        Debug.Log("OnOwnershipRequest(): Player " + requestingPlayer + " requests ownership of: " + view + ".");
+
+        if (!lockedViews.Contains(view))
+        {
+            view.TransferOwnership(requestingPlayer);
+        }
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipRequester.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnershipRequester : MonoBehaviourPun
+{
+    public void OnManipulationStarted()
+    {
+        if (photonView.Owner == PhotonNetwork.LocalPlayer)
+        {
+            Debug.Log("Locking object", gameObject);
+            OwnershipManager.Instance.DisableOwnerShipTransfer(photonView);
+        }
+        else
+        {
+            Debug.Log("Requesting ownership", gameObject);
+            photonView.RequestOwnership();
+        }
+    }
+
+    public void OnManipulationEnded()
+    {
+        if (photonView.Owner == PhotonNetwork.LocalPlayer)
+        {
+            OwnershipManager.Instance.EnableOwnerShipTransfer(photonView);
+        }
+    }
+}
