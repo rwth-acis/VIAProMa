e375cf8aecc88319b909919f3959f5119eebc93e
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-01 22:58:04 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/WebConnection/NetworkedStringManager.cs
+++ Frontend/VIAProMa/Assets/Scripts/WebConnection/NetworkedStringManager.cs
@@ -5,10 +5,20 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 
+/// <summary>
+/// Manager for sharing strings
+/// The manager converts strings to numeric ids which can be sent over network
+/// It also handles the conversion back from ids to strings.
+/// </summary>
 public static class NetworkedStringManager
 {
     private const string serviceEndpoint = "networkedStrings";
 
+    /// <summary>
+    /// Converts a string to a networked id which can be sent in the sharing service
+    /// </summary>
+    /// <param name="text">The text to convert</param>
+    /// <returns>The networked id</returns>
     public static async Task<short> StringToId(string text)
     {
         if (string.IsNullOrEmpty(text))
@@ -31,6 +41,11 @@ public static class NetworkedStringManager
         }
     }
 
+    /// <summary>
+    /// Converts a text id to the corresponding string
+    /// </summary>
+    /// <param name="id">The id which identifies the text segment</param>
+    /// <returns>The text segment of the id</returns>
     public static async Task<string> GetString(short id)
     {
         Response resp = await Rest.GetAsync(ConnectionManager.Instance.BackendAPIBaseURL + serviceEndpoint + "/" + id);
