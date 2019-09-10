72cf9e28cf1c57e1b8357ef37bd6b023bef115f8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-10 16:28:40 +0200

    grid of Kanban board column now supports deactivated gameobjects

--- Frontend/VIAProMa/Assets/Scripts/Utilities/ObjectGrid.cs
+++ Frontend/VIAProMa/Assets/Scripts/Utilities/ObjectGrid.cs
@@ -16,23 +16,39 @@ public class ObjectGrid : MonoBehaviour
 
     public void UpdateGrid()
     {
+        int activeChildCount = 0;
+        foreach(Transform child in transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                activeChildCount++;
+            }
+        }
+
         Vector2 offset = Vector2.zero;
         if (Centered)
         {
             offset.x = (Columns - 1) * CellSize.x / 2f;
-            int rows = Mathf.CeilToInt(transform.childCount / Columns);
+            int rows = Mathf.CeilToInt(activeChildCount / Columns);
             offset.y = -(rows-1) * CellSize.y / 2f;
         }
 
         int row, column;
+        int iForActiveObjects = 0;
         for (int i = 0; i < transform.childCount; i++)
         {
-            row = i / Columns;
-            column = i % Columns;
-            transform.GetChild(i).localPosition = new Vector3(
-                CellSize.x * column - offset.x,
-                CellSize.y * (-row) - offset.y,
-                0f);
+            row = iForActiveObjects / Columns;
+            column = iForActiveObjects % Columns;
+            Transform child = transform.GetChild(i);
+            if (child.gameObject.activeSelf)
+            {
+                child.localPosition = new Vector3(
+                    CellSize.x * column - offset.x,
+                    CellSize.y * (-row) - offset.y,
+                    0f);
+
+                iForActiveObjects++;
+            }
         }
     }
 }
