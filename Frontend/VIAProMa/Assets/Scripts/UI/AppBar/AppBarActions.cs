6063f9d5a840c1a9f6d7a2d74416ef5b8e8c978e
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-16 21:00:58 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/UI/AppBar/AppBarActions.cs
+++ Frontend/VIAProMa/Assets/Scripts/UI/AppBar/AppBarActions.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Contains the application logic and actions which are triggered if the buttons on the app bar are pressed
+/// </summary>
 [RequireComponent(typeof(AppBarPlacer))]
 public class AppBarActions : MonoBehaviour
 {
@@ -10,9 +13,16 @@ public class AppBarActions : MonoBehaviour
 
     private Vector3 startPosition;
     private Quaternion startRotation;
+    private Vector3 startScale;
 
+    /// <summary>
+    /// True if the target to which the app bar belongs is a networed object (with a PhotonView)
+    /// </summary>
     public bool TargetNetworked { get; set; }
 
+    /// <summary>
+    /// Checks the component's setup and initializes it
+    /// </summary>
     private void Awake()
     {
         appBarPlacer = GetComponent<AppBarPlacer>();
@@ -22,6 +32,10 @@ public class AppBarActions : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Destroys the object (either networked or not based on the setting TargetNetworked)
+    /// This also destroys the bounding box and finally the app bar
+    /// </summary>
     public void RemoveObject()
     {
         if (TargetNetworked)
@@ -42,15 +56,23 @@ public class AppBarActions : MonoBehaviour
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Puts the app bar into placement mode and stores the current position, rotation and scale for the reset option
+    /// </summary>
     public void StartAdjustment()
     {
         startPosition = appBarPlacer.TargetBoundingBox.Target.transform.localPosition;
         startRotation = appBarPlacer.TargetBoundingBox.Target.transform.localRotation;
+        startScale = appBarPlacer.TargetBoundingBox.Target.transform.localScale;
     }
 
+    /// <summary>
+    /// Resets the target object to its position, rotation and scale to the status when the adjustment was started
+    /// </summary>
     public void ResetAdjustment()
     {
         appBarPlacer.TargetBoundingBox.Target.transform.localPosition = startPosition;
         appBarPlacer.TargetBoundingBox.Target.transform.localRotation = startRotation;
+        appBarPlacer.TargetBoundingBox.Target.transform.localScale = startScale;
     }
 }
