bf0071c765a4f79dacad894907f8d8ff4b274119
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-22 16:34:20 +0200

    added bounding box to issue cards

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipRequester.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/OwnershipRequester.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class OwnershipRequester : MonoBehaviourPun
 {
-    public void OnManipulationStarted()
+    public void EnsureOwnership()
     {
         // since we are dealing with scene objects, there is no owner in the beginning
         if (photonView.Owner != null)
@@ -28,7 +28,7 @@ public class OwnershipRequester : MonoBehaviourPun
         }
     }
 
-    public void OnManipulationEnded()
+    public void ReleaseOwnershipLock()
     {
         if (photonView.Owner == PhotonNetwork.LocalPlayer)
         {
