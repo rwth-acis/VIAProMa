57e92f864c22d4a65f944f64b83d49d16cec537d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-22 22:51:49 +0200

    synchronized filter is now also updated in configuration UI

--- Frontend/VIAProMa/Assets/Scripts/Visualizations/CompetenceDisplay/CompetenceDisplayConfiguration.cs
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/CompetenceDisplay/CompetenceDisplayConfiguration.cs
@@ -7,9 +7,12 @@ using UnityEngine;
 [RequireComponent(typeof(ConfigurationIssueSelectionUI))]
 public class CompetenceDisplayConfiguration : ConfigurationWindow
 {
+    [Header("UI Elements")]
     [SerializeField] private InputField filterInputField;
 
     private ConfigurationIssueSelectionUI issueSelection;
+
+    private bool externalConfiguration = false;
 
     public override bool WindowEnabled
     {
@@ -41,13 +44,28 @@ public class CompetenceDisplayConfiguration : ConfigurationWindow
         ((CompetenceDisplay)visualization).FilterWords = new string[0];
     }
 
+    private void OnEnable()
+    {
+        ((CompetenceDisplay)visualization).FilterChanged += FilterExternallyChanged;
+    }
+
+    private void OnDisable()
+    {
+        ((CompetenceDisplay)visualization).FilterChanged -= FilterExternallyChanged;
+    }
+
     private void FilterChanged(object sender, EventArgs e)
     {
+        if (externalConfiguration)
+        {
+            return;
+        }
+
         string filterText = filterInputField.Text;
         // separate by ";"
         string[] filter = filterText.Split(';');
         // clean up whitespace characters and make everything lower case
-        for (int i=0;i<filter.Length;i++)
+        for (int i = 0; i < filter.Length; i++)
         {
             filter[i] = filter[i].Trim();
             filter[i] = filter[i].ToLowerInvariant();
@@ -56,5 +74,24 @@ public class CompetenceDisplayConfiguration : ConfigurationWindow
         // apply filter
         ((CompetenceDisplay)visualization).FilterWords = filter;
         visualization.UpdateView();
+    }
+
+    private void FilterExternallyChanged(object sender, EventArgs e)
+    {
+        externalConfiguration = true;
+
+        string[] filterWords = ((CompetenceDisplay)visualization).FilterWords;
+        string combinedFilter = "";
+        for (int i=0;i<filterWords.Length;i++)
+        {
+            combinedFilter += filterWords[i];
+            if (i < filterWords.Length -1)
+            {
+                combinedFilter += "; ";
+            }
+        }
+        filterInputField.Text = combinedFilter;
+
+        externalConfiguration = false;
     }
 }
