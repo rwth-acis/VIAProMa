5e080db187e8b1c8f603ee5675b1c3b16a4dcb6e
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2019-09-26 02:04:19 +0200

    added documentation

--- Frontend/VIAProMa/Assets/Scripts/Multiplayer/Synchronizer/CommitStatisticsSynchronizer.cs
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Synchronizer/CommitStatisticsSynchronizer.cs
@@ -5,6 +5,9 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 
+/// <summary>
+/// Synchronizes the commit statistics visualization
+/// </summary>
 [RequireComponent(typeof(CommitStatisticsVisualizer))]
 public class CommitStatisticsSynchronizer : TransformSynchronizer
 {
@@ -13,11 +16,17 @@ public class CommitStatisticsSynchronizer : TransformSynchronizer
     private int remoteSynchronization = 0;
     private bool initialized;
 
+    /// <summary>
+    /// Gets the CommitStatisticsVisualizer
+    /// </summary>
     private void Awake()
     {
         visualizer = GetComponent<CommitStatisticsVisualizer>();
     }
 
+    /// <summary>
+    /// Checks if the client is the master client and initializes itself and other participants
+    /// </summary>
     private void Start()
     {
         if (PhotonNetwork.IsMasterClient)
@@ -27,6 +36,11 @@ public class CommitStatisticsSynchronizer : TransformSynchronizer
         }
     }
 
+    /// <summary>
+    /// Called by Photon if a player joins the room
+    /// If the current client is the master client, it will send the configuration data to initialize the new client
+    /// </summary>
+    /// <param name="newPlayer">The player who joined the room</param>
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         if (PhotonNetwork.IsMasterClient && newPlayer.UserId != PhotonNetwork.LocalPlayer.UserId)
@@ -35,12 +49,20 @@ public class CommitStatisticsSynchronizer : TransformSynchronizer
         }
     }
 
+    /// <summary>
+    /// Called when the component becomes active
+    /// Subscribes to the events of the CommitStatisticsVisualizer
+    /// </summary>
     public override void OnEnable()
     {
         base.OnEnable();
         visualizer.ConfigurationChanged += OnConfigurationChanged;
     }
 
+    /// <summary>
+    /// Called when the component becomes inactive
+    /// Un-subscribes to the events of the CommitStatsticsVisualizer
+    /// </summary>
     public override void OnDisable()
     {
         base.OnDisable();
@@ -48,11 +70,20 @@ public class CommitStatisticsSynchronizer : TransformSynchronizer
 
     }
 
+    /// <summary>
+    /// Called by the configuration changed event of the CommitStatisticsVisualizer
+    /// </summary>
+    /// <param name="sender">The sender of the event</param>
+    /// <param name="e">Event arguments</param>
     private void OnConfigurationChanged(object sender, EventArgs e)
     {
+        // inform others about the changed configuration
         SendConfiguration();
     }
 
+    /// <summary>
+    /// Sends the current configuration of the visualizer to the other clients
+    /// </summary>
     private async void SendConfiguration()
     {
         short ownerId = await NetworkedStringManager.StringToId(visualizer.Owner);
@@ -60,6 +91,12 @@ public class CommitStatisticsSynchronizer : TransformSynchronizer
         photonView.RPC("SetConfiguration", RpcTarget.Others, ownerId, repositoryId);
     }
 
+    /// <summary>
+    /// Called remotely by clients in the room
+    /// Receives the configuration for the visualizer and applies the settings
+    /// </summary>
+    /// <param name="ownerId">The string id of the repository owner</param>
+    /// <param name="repositoryId">The string id of the repository name</param>
     [PunRPC]
     private async void SetConfiguration(short ownerId, short repositoryId)
     {
