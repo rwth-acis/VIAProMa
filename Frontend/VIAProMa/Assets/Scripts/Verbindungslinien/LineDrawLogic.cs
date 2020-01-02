9cc57844a99ac89cb62dd388c2b1d8388a1b0fd5
JulianStaab <julianstaab@web.de>
2020-01-02 18:43:38 +0100

    Added comments to LineDrawLogic

--- Frontend/VIAProMa/Assets/Scripts/Verbindungslinien/LineDrawLogic.cs
+++ Frontend/VIAProMa/Assets/Scripts/Verbindungslinien/LineDrawLogic.cs
@@ -5,21 +5,41 @@ using TMPro;
 
 public class LineDrawLogic : MonoBehaviour
 {
+    /// <summary>
+    /// Referencing the caption of the button
+    /// </summary>
     public GameObject caption;
+
+    /// <summary>
+    /// Referencing the LineDraw Button
+    /// </summary>
+    public GameObject lineDrawButton;
+
+    /// <summary>
+    /// True, if the LineDraw Mode is active
+    /// </summary>
     private bool isLineModeActivated;
-    // Start is called before the first frame update
+
+    /// <summary>
+    /// Start with the button invisible and the LineDraw Mode is deactivated.
+    /// </summary>
     void Start()
     {
-        gameObject.SetActive(false);
+        lineDrawButton.SetActive(false);
         isLineModeActivated = false;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// The LineDraw Button appears when the Menu is open and is deactivated when it is closed.
+    /// </summary>
     void Update()
     {
-        gameObject.SetActive(transform.parent.parent.parent.parent.parent.GetComponent<FoldController>().MenuOpen);
+        gameObject.SetActive(transform.parent.parent.parent.parent.GetComponent<FoldController>().MenuOpen);
     }
 
+    /// <summary>
+    /// Is called when the LineDraw Button is clicked. Enables/Disables the LineDrawingMode and switches the displayed text accordingly.
+    /// </summary>
     public void SwitchLineDrawMode()
     {
         if(isLineModeActivated)
