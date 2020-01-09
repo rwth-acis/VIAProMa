0955dbb976f5f797c14436d3a312137984da4166
stevenzambr <stevenzambr@gmail.com>
2020-01-09 22:24:02 +0100

    Added UI Sample

--- Frontend/VIAProMa/Assets/Scripts/moveObjectToGaze.cs
+++ Frontend/VIAProMa/Assets/Scripts/moveObjectToGaze.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 
-public class moveObjectToGaze : MonoBehaviour
+public class moveObjectToGaze : MonoBehaviour, IMixedRealityPointerHandler
 {
-    private Vector3 up = new Vector3(0f,0.1f,-0.1f);
+    private Vector3 up = new Vector3(0f,0.1f,0f);
     private Vector3 far = new Vector3(0f,-10f,0f);
     Material mat;
+    public Text txt;
 
     void Start()
     {
@@ -19,22 +21,15 @@ public class moveObjectToGaze : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log("Current GazePointer : " + giveGaze());
-        /*print("Current InputSystemProfile: " + MixedRealityToolkit.InputSystem.InputSystemProfile);
-        Debug.Log("Current IsInputEnabled : " + MixedRealityToolkit.InputSystem.IsInputEnabled);
-        Debug.Log("Current GazeCursor : " + MixedRealityToolkit.InputSystem.GazeProvider.GazeCursor.IsVisible);
-        Debug.Log("Current GazePointer : " + MixedRealityToolkit.InputSystem.GazeProvider.GazePointer);*/
-        //Debug.Log("Detected Controllers : " + MixedRealityToolkit.InputSystem.DetectedControllers);
-        //Debug.Log("Enabled ? : " + MixedRealityToolkit.InputSystem.GazeProvider.Enabled);
-        //Debug.Log("Pointer ? : " + MixedRealityToolkit.InputSystem);
-        //Debug.Log("Direction : " + giveGaze().GazeDirection);
-        //print("Current Target : " + giveGaze().GazeTarget);
+        //Debug.Log("Current GameObjectReference : " + giveGaze().GameObjectReference.name);
         if (giveGaze().GazeTarget)
         {
             Vector3 currentHitPosition = giveGaze().HitPosition;
             gameObject.transform.position = currentHitPosition + up;
             Vector3 angle = new Vector3(gameObject.transform.eulerAngles.x, giveGaze().GazeDirection.x * 90, gameObject.transform.eulerAngles.z);
             gameObject.transform.eulerAngles = angle;
+            txt.text = giveGaze().GazeTarget.name;
+            
         } else {gameObject.transform.position = far;}
     }
 
@@ -43,5 +38,33 @@ public class moveObjectToGaze : MonoBehaviour
         IMixedRealityGazeProvider currentGaze = MixedRealityToolkit.InputSystem.GazeProvider;
         return currentGaze;
         
-    } 
+    }
+
+    /*public IMixedRealityGazeProvider giveGGV()
+    {
+        IMixedRealityGazeProvider currentGaze = MixedRealityToolkit.InputSystem.GazeProvider;
+        return currentGaze;
+
+    }
+    */
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
 }
---
+++ Frontend/VIAProMa/Assets/Scripts/shareGaze2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shareGaze2 : MonoBehaviour
+{
+    public Material mat;
+
+    public void toggleColor()
+    {
+        if (mat.color == Color.black)
+        {
+            mat.color = Color.blue;
+        }
+        else { mat.color = Color.black; }
+    }
+}
