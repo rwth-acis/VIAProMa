831bd36fac5e04c9214179ee143d44397f2db816
JulianStaab <julianstaab@web.de>
2020-01-11 21:16:16 +0100

    Enhanced the testscene

---
+++ Frontend/VIAProMa/Assets/Tests/Verbindungslinien/LineDrawLogicTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawLogicTester : MonoBehaviour
+{
+    public LineDrawLogic linedrawlogicscript;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            linedrawlogicscript.SwitchLineDrawMode();
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            linedrawlogicscript.DeleteAllLines();
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            linedrawlogicscript.DeleteSpecificLine();
+        }
+    }
+}
