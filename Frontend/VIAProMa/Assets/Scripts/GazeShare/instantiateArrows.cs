c02f1daab7d369c0234dedc998700eff65bb0fe1
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-01-11 15:33:01 +0100

    Prefab Resources version of the arrow; Script instantiateArrows

---
+++ Frontend/VIAProMa/Assets/Scripts/GazeShare/instantiateArrows.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class instantiateArrows : MonoBehaviourPun, IPunObservable
+{
+
+    protected Vector3 targetPosition;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            Vector3 pos = transform.localPosition;
+            stream.Serialize(ref pos);
+        }
+        else
+        {
+            targetPosition = (Vector3)(stream.ReceiveNext());
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate("arrow2", new Vector3(0, 0.5f, 1.45f), Quaternion.identity, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(photonView.IsMine))
+        {
+            transform.position = targetPosition;
+        }
+    }
+}
