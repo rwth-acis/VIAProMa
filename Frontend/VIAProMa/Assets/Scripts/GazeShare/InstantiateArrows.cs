9bf9571ade58d2852aaa14e49c1f9f3bfb8904e4
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-01-20 13:16:19 +0100

    Rotation setting attempt

--- Frontend/VIAProMa/Assets/Scripts/GazeShare/InstantiateArrows.cs
+++ Frontend/VIAProMa/Assets/Scripts/GazeShare/InstantiateArrows.cs
@@ -59,13 +59,13 @@ public class InstantiateArrows : MonoBehaviourPun, IPunObservable
         {
             // Copy rotation of DefaultCursor
             GameObject target = GameObject.FindGameObjectWithTag("cursor");
-            Quaternion newRotation = target.transform.rotation;
-            // Vector3 newRotation = new Vector3(target.transform.eulerAngles.x, target.transform.eulerAngles.y, target.transform.eulerAngles.z);
-            // transform.eulerAngles = newRotation;
+            //Quaternion newRotation = target.transform.rotation;
+            Vector3 newRotation = new Vector3(target.transform.eulerAngles.x - 90f, target.transform.eulerAngles.y - 180f, target.transform.eulerAngles.z);
 
             Vector3 currentHitPosition = MixedRealityToolkit.InputSystem.GazeProvider.HitPosition;
             transform.position = currentHitPosition + up;
-            transform.rotation = newRotation;
+            //transform.rotation = Quaternion.Inverse(newRotation);
+            transform.eulerAngles = newRotation;
             GetComponentInChildren<TextMeshPro>().text = "Hololens";
         }
         else if (getIsUsingVive() == true && sharing == true && sharingGlobal == true)
