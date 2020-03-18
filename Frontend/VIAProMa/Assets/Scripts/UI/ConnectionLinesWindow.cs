ebfacdac56f8b9c69ecc3eb2c3daeae31c11dfc1
EmilieHK <EmilieHK@users.noreply.github.com>
2020-03-18 12:54:46 +0100

    Creating ConnectionLine Window
    Incomplete adjustment of the new ConnectionLine Window.

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/ConnectionLinesWindow.cs
@@ -0,0 +1,37 @@
+﻿using i5.ViaProMa.UI;
+using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionLinesWindow : MonoBehaviour, IWindow
+{
+    public bool WindowEnabled { get; set; }
+
+    public bool WindowOpen
+    {
+        get => gameObject.activeSelf;
+    }
+
+    public event EventHandler WindowClosed;
+
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        WindowClosed?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Open(Vector3 position, Vector3 eulerAngles)
+    {
+        Open();
+        transform.localPosition = position;
+        transform.localEulerAngles = eulerAngles;
+    }
+}
