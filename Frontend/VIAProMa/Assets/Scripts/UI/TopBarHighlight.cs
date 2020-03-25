83b1293fd583bef1e50683807f48bc2982606831
EmilieHK <EmilieHK@users.noreply.github.com>
2020-03-25 15:36:31 +0100

    Added Highlights to TopBar
    The top bar changes color when in focus. Color not tested yet.

---
+++ Frontend/VIAProMa/Assets/Scripts/UI/TopBarHighlight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class TopBarHighlight : MonoBehaviour , IMixedRealityFocusHandler
+{
+    [SerializeField] Renderer backgroundRenderer;
+
+    private Color defaultColor;
+    private Color highlightColor = Color.red;
+
+    private void Awake()
+    {
+        backgroundRenderer = gameObject.GetComponent<Renderer>();
+        defaultColor = backgroundRenderer.material.color;
+
+    }
+
+
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        backgroundRenderer.material.color = highlightColor;
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        backgroundRenderer.material.color = defaultColor;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        backgroundRenderer.material.color = defaultColor;
+    }
+}
