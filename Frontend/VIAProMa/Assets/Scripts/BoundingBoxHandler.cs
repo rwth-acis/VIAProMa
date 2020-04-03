246663da9f558896f56651b60b055de867dac880
EmilieHK <EmilieHK@users.noreply.github.com>
2020-04-03 16:40:16 +0200

    Adjust Button changes Caption
    Depending on the bounding box being active or not, the caption of the controlling button says "Done" or "Adjust".

--- Frontend/VIAProMa/Assets/Scripts/BoundingBoxHandler.cs
+++ Frontend/VIAProMa/Assets/Scripts/BoundingBoxHandler.cs
@@ -5,12 +5,14 @@ using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using Microsoft.MixedReality.Toolkit.UI;
 using System;
+using TMPro;
 
 public class BoundingBoxHandler : MonoBehaviour
 {
 
     [SerializeField] private BoundingBox boundingBox;
     [SerializeField] private Collider collider;
+    [SerializeField] private GameObject adjustButtonCaption;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +26,15 @@ public class BoundingBoxHandler : MonoBehaviour
     // Update is called once per frame
     public void Adjust()
     {
-        collider.enabled = true;
-        boundingBox.Active = true;
+        if (boundingBox.Active)
+        {
+            adjustButtonCaption.GetComponent<TextMeshPro>().SetText("Adjust");
+        }
+        else
+        {
+            adjustButtonCaption.GetComponent<TextMeshPro>().SetText("Done");
+        }
+        collider.enabled = !collider.enabled;
+        boundingBox.Active = !boundingBox.Active;
     }
 }
