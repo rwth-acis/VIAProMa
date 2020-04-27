3b5d2fc8052502b684831a951fa0d5067902b10b
Teng0927 <asd25877@gmail.com>
2020-04-27 16:05:37 +0200

    information box hide/visible setting

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/Configurator/InformationBoxConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using i5.ViaProMa.UI;
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class InformationBoxConfigurator : MonoBehaviour
+{
+    [SerializeField] private GameObject informationBox;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        informationBox.SetActive(false);
+    }
+
+    public void Open()
+    {
+        informationBox.SetActive(true);
+    }
+
+    public void Close()
+    {
+        informationBox.SetActive(false);
+    }
+
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Avatars/Customization System/Configurator/InformationButtonConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using i5.ViaProMa.UI;
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class InformationButtonConfigurator : MonoBehaviour
+{
+    [SerializeField] private GameObject informationButton;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        informationButton.SetActive(true);
+    }
+
+    public void Open()
+    {
+        informationButton.SetActive(true);
+    }
+
+    public void Close()
+    {
+        informationButton.SetActive(false);
+    }
+
+}
