9641dcf1a371e7e61583bb9f5238e2aac905e00c
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-06-18 16:16:12 +0200

    The constructor now initiallises the dictionary

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/Classes/ClusterClass.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/Classes/ClusterClass.cs
@@ -7,6 +7,11 @@ public class Cluster
     //The entraces on the cluster side (1,0,0) for example would be the entraces on the plane that is in direction (1,0,0) from the middle of the cluster.
     private Dictionary<IntTriple, List<Vector3>> clusterSides;
 
+    public Cluster()
+    {
+        clusterSides = new Dictionary<IntTriple, List<Vector3>>();
+    }
+
     public List<Vector3> getEntrances(IntTriple planeDirection)
     {
         if (clusterSides.ContainsKey(planeDirection))
