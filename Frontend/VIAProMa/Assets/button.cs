52469615ad95f20578dcaf80bbf7165568719e54
ElcinKonar <elcin-havva.konar@rwth-aachen.de>
2020-06-21 00:36:29 +0200

    aktualisiert

---
+++ Frontend/VIAProMa/Assets/button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void ausgabe()
+
+    {
+        Debug.Log("Test");
+        
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+}
