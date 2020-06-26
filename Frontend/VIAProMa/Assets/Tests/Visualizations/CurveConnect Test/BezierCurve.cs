5c72e7b7441d07581c11d7e6fc9a18f2441ed6a4
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-06-26 19:21:26 +0200

    Added general Bezier Calculation

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/BezierCurve.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/BezierCurve.cs
@@ -10,14 +10,14 @@ public class BezierCurve
         for (int i = 1; i <= segmentCount; i++)
         {
             float t = i / (float)segmentCount;
-            Vector3 point = CalculateCubicBezierPoint(t, p1, p2, p3, p4);
-            curve[i - 1] = point;
+            Vector3 Vector3 = CalculateCubicBezierVector3(t, p1, p2, p3, p4);
+            curve[i - 1] = Vector3;
         }
 
         return curve;
     }
 
-    static Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    static Vector3 CalculateCubicBezierVector3(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
         float u = 1 - t;
         float tt = t * t;
@@ -31,5 +31,40 @@ public class BezierCurve
         p += ttt * p3;
 
         return p;
+    }
+
+    public static Vector3[] calculateCurve(Vector3[] controlPoints, int segmentCount)
+    {
+        Vector3[] points = new Vector3[segmentCount + 1];
+        for (int i = 0; i <= segmentCount; i++)
+        {
+            float t = (float)i / segmentCount;
+            points[i] = CalculateBezierPoint(controlPoints,t);
+        }
+        return points;
+    }
+
+    static Vector3 CalculateBezierPoint(Vector3[] controlPoints, float t)
+    {
+        Vector3 result = Vector3.zero;
+        for (int i = 0; i < controlPoints.Length; i++)
+        {
+            result += (float)(BinomCoefficient(controlPoints.Length-1, i) * Math.Pow(1 - t, controlPoints.Length-1 - i) * Math.Pow(t, i)) * controlPoints[i];
+        }
+        return result;
+    }
+
+    public static long BinomCoefficient(long n, long k)
+    {
+        if (k > n) { return 0; }
+        if (n == k) { return 1; } // only one way to chose when n == k
+        if (k > n - k) { k = n - k; } // Everything is symmetric around n-k, so it is quicker to iterate over a smaller k than a larger one.
+        long c = 1;
+        for (long i = 1; i <= k; i++)
+        {
+            c *= n--;
+            c /= i;
+        }
+        return c;
     }
 }
