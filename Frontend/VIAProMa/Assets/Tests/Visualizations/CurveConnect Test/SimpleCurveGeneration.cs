2761d9f308f0d2986da364c11424d8a64a7e6505
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-07-02 01:00:13 +0200

    Th side path for the curve now also use the improved joined bezier Method

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/SimpleCurveGeneration.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/SimpleCurveGeneration.cs
@@ -76,12 +76,6 @@ public class SimpleCurveGerneration
         return false;
     }
 
-    enum Direction
-    {
-        Left,
-        Right
-    }
-
     public static Vector3[] startContinous(Vector3 start, Vector3 goal, GameObject[] pointVis)
     {
         
@@ -147,55 +141,7 @@ public class SimpleCurveGerneration
 
         if (distanceAbove < 1.5 * distanceSide)
         {
-            /*
-            controllPoints = new Vector3[6];
-            controllPoints[0] = start;
-            controllPoints[5] = goal;
-            //Shift them away from the obstacle to prevent collisions
-            controllPoints[1] = intersectionPointsAbove[0] + new Vector3(0, 1 + (maxPoint.y - startAdjusted.y) * 0.2f, 0) - direction * (distance / 2) / (5 + Vector3.Distance(start, new Vector3(controllPoints[1].x, start.y, controllPoints[1].z)));
-            controllPoints[4] = intersectionPointsAbove[1] + new Vector3(0, 1 + (maxPoint.y - startAdjusted.y) * 0.2f, 0) + direction * (distance / 2) / (5 + Vector3.Distance(goal, new Vector3(controllPoints[4].x, start.y, controllPoints[4].z)));
-
-            controllPoints[2] = controllPoints[1] + new Vector3(0, 2, 0) - 2 * direction;
-            controllPoints[3] = controllPoints[4] + new Vector3(0, 2, 0) + 2 * direction;
-
-            //pointVis[1].transform.position = controllPoints[1];
-            //pointVis[2].transform.position = controllPoints[2];
-            //pointVis[3].transform.position = controllPoints[3];
-            //pointVis[4].transform.position = controllPoints[4];
-            */
-
-            Vector3[] controllPointsCurve1 = new Vector3[3];
-            Vector3[] controllPointsCurve2 = new Vector3[4];
-            Vector3[] controllPointsCurve3 = new Vector3[3];
-
-            //Curve 1
-            controllPointsCurve1[0] = start;
-            controllPointsCurve1[2] = intersectionPointsAbove[0];
-            //cp Lies in the centroid of the trianlgle spanned by cPC1[0],cPC1[2] and the point (cPC1[0].x, cPC1[2].y, cPC1[0].x.z)
-            controllPointsCurve1[1] = CalculateCentroid(controllPointsCurve1[0], controllPointsCurve1[2]);
-
-            //Curve 3
-            controllPointsCurve3[0] = intersectionPointsAbove[1];
-            controllPointsCurve3[2] = goal;
-            controllPointsCurve3[1] = CalculateCentroid(controllPointsCurve3[2], controllPointsCurve3[0]);
-
-            //Curve 2 that connects Curve 1 and 3
-            controllPointsCurve2[0] = controllPointsCurve1[2];
-            controllPointsCurve2[3] = controllPointsCurve3[0];
-
-            Vector3 aboveMiddle = CalculateLineIntersection(controllPointsCurve1[1], controllPointsCurve1[2] - controllPointsCurve1[1], controllPointsCurve3[1], controllPointsCurve3[0] - controllPointsCurve3[1]);
-            controllPointsCurve2[1] = controllPointsCurve2[0] + (aboveMiddle - controllPointsCurve2[0]) / 2;
-            controllPointsCurve2[2] = controllPointsCurve2[3] + (aboveMiddle - controllPointsCurve2[3]) / 2;
-
-            Vector3[] curve1 = BezierCurve.calculateCurve(controllPointsCurve1, 20);
-            Vector3[] curve2 = BezierCurve.calculateCurve(controllPointsCurve2, 20);
-            Vector3[] curve3 = BezierCurve.calculateCurve(controllPointsCurve3, 20);
-            Vector3[] curve = new Vector3[curve1.Length + curve2.Length + curve3.Length];
-            curve1.CopyTo(curve,0);
-            curve2.CopyTo(curve, curve1.Length);
-            curve3.CopyTo(curve, curve1.Length+ curve2.Length);
-
-            return curve;
+            return CalculateJoinedCurve(start, intersectionPointsAbove, goal, Vector3.up);
         }
 
         else
@@ -210,11 +156,10 @@ public class SimpleCurveGerneration
             }
             else
             {
-                controllPoints = new Vector3[4];
-                controllPoints[0] = start;
-                controllPoints[1] = shiftAway(intersectionPointsSide[0], boundingBoxCenterShifted);
-                controllPoints[2] = shiftAway(intersectionPointsSide[1], boundingBoxCenterShifted);
-                controllPoints[3] = goal;
+                Vector3 up = intersectionPointsSide[0] - boundingBoxCenterShifted + intersectionPointsSide[1] - boundingBoxCenterShifted;
+                up.Normalize();
+
+                return CalculateJoinedCurve(start, intersectionPointsSide, goal, up);
             }
 
         }
@@ -380,25 +325,68 @@ public class SimpleCurveGerneration
         return FloatEq(vec1.x, vec2.x) && FloatEq(vec1.y, vec2.y) && FloatEq(vec1.z, vec2.z);
     }
 
-    static Vector3 CalculateCentroid(Vector3 p0, Vector3 p2)
+    static Vector3 CalculateCentroid(Vector3 p0, Vector3 p1, Vector3 p2)
     {
-        Vector3 p1 = new Vector3(p0.x, p2.y, p0.z);
-
         Vector3 p01 = p1 + (p0 - p1) / 2;
         Vector3 p12 = p2 + (p1 - p2) / 2;
         Vector3 d1 = p2 - p01;
         Vector3 d2 = p0 - p12;
 
-        //float r = (p12.y - p01.y - ((d1.y * p12.x + p01.x) / (d1.x))) / (d2.x / d1.x - d2.y);
-
-        //return p12 + r*d2;
         return CalculateLineIntersection(p01, d1, p12, d2);
     }
 
     static Vector3 CalculateLineIntersection(Vector3 startPoint1, Vector3 direction1, Vector3 startPoint2, Vector3 direction2)
     {
-        //float r = (startPoint2.y - startPoint1.y - ((direction1.y * startPoint2.x + startPoint1.x) / (direction1.x))) / (direction2.x / direction1.x - direction2.y);
         float r = (startPoint2.y-startPoint1.y- (direction1.y*(startPoint2.x - startPoint1.x) / (direction1.x))) / ((direction1.y*direction2.x)/(direction1.x) - direction2.y);
         return startPoint2 + r * direction2;
+    }
+
+    static Vector3[] CalculateJoinedCurve(Vector3 start, Vector3[] middlePoints, Vector3 goal, Vector3 up)
+    {
+        Vector3[] controllPointsCurve1 = new Vector3[3];
+        Vector3[] controllPointsCurve2 = new Vector3[4];
+        Vector3[] controllPointsCurve3 = new Vector3[3];
+
+        Vector3 direction = middlePoints[1] - middlePoints[0];
+        direction.Normalize();
+        middlePoints[0] -= direction;
+        middlePoints[0] += up;
+        middlePoints[1] += direction;
+        middlePoints[1] += up;
+
+        //Curve 1
+        controllPointsCurve1[0] = start;
+        controllPointsCurve1[2] = middlePoints[0];
+        float hypothenuse = Vector3.Distance(controllPointsCurve1[0], controllPointsCurve1[2]);
+        float alpha = (float)(Math.PI * Vector3.Angle(up, controllPointsCurve1[2] - controllPointsCurve1[0]) /180);
+        Vector3 p1 = controllPointsCurve1[0] + up * (float)(hypothenuse * Math.Cos(alpha));
+        controllPointsCurve1[1] = CalculateCentroid(controllPointsCurve1[0], p1 , controllPointsCurve1[2]);
+
+
+
+        //Curve 3
+        controllPointsCurve3[0] = middlePoints[1];
+        controllPointsCurve3[2] = goal;
+        hypothenuse = Vector3.Distance(controllPointsCurve3[0], controllPointsCurve3[2]);
+        alpha = (float)(Math.PI * Vector3.Angle(up, controllPointsCurve3[0] - controllPointsCurve3[2]) / 180);
+        controllPointsCurve3[1] = CalculateCentroid(controllPointsCurve3[2], controllPointsCurve3[2] + up * hypothenuse * (float)Math.Cos(alpha), controllPointsCurve3[0]);
+
+        //Curve 2 that connects Curve 1 and 3
+        controllPointsCurve2[0] = controllPointsCurve1[2];
+        controllPointsCurve2[3] = controllPointsCurve3[0];
+
+        Vector3 aboveMiddle = CalculateLineIntersection(controllPointsCurve1[1], controllPointsCurve1[2] - controllPointsCurve1[1], controllPointsCurve3[1], controllPointsCurve3[0] - controllPointsCurve3[1]);
+        controllPointsCurve2[1] = controllPointsCurve2[0] + (aboveMiddle - controllPointsCurve2[0]) / 2;
+        controllPointsCurve2[2] = controllPointsCurve2[3] + (aboveMiddle - controllPointsCurve2[3]) / 2;
+
+        Vector3[] curve1 = BezierCurve.calculateCurve(controllPointsCurve1, 20);
+        Vector3[] curve2 = BezierCurve.calculateCurve(controllPointsCurve2, 20);
+        Vector3[] curve3 = BezierCurve.calculateCurve(controllPointsCurve3, 20);
+        Vector3[] curve = new Vector3[curve1.Length + curve2.Length + curve3.Length];
+        curve1.CopyTo(curve, 0);
+        curve2.CopyTo(curve, curve1.Length);
+        curve3.CopyTo(curve, curve1.Length + curve2.Length);
+
+        return curve;
     }
 }
