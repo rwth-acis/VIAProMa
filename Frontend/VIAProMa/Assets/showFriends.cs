c2bceb34d48fcf78fa130c4c4da2a73f1cfb58cb
ElcinKonar <elcin-havva.konar@rwth-aachen.de>
2020-07-09 20:42:16 +0200

    friendlist

---
+++ Frontend/VIAProMa/Assets/showFriends.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showFriends : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+ FriendList friendList = new FriendList();
+ DeleteButton deleteButton = new DeleteButton();
+
+    void showFriendList(friendList)
+    {
+        //bezug auf hannahs code
+        if (participant = friend)
+        {
+            participant.add(friendList);
+
+        }
+        return friendList;
+    }
+
+    void deleteFriends(friendList)
+
+        {
+        if (deleteButton = true)
+            deleteFriends;
+        }
+return friendList;
+    }
+}
