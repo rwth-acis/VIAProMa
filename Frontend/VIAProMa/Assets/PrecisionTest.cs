702dd38a1ad0ae070bd9c75ed430825f7d94b888
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-08-20 12:49:07 +0300

    Set up 2D distance measurement

--- Frontend/VIAProMa/Assets/PrecisionTest.cs
+++ Frontend/VIAProMa/Assets/PrecisionTest.cs
@@ -13,8 +13,30 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         DateTime time = eventData.EventTime;
         IMixedRealityInputSource source = eventData.InputSource;
         Vector3 centerPosition = center.transform.localPosition;
-        float distance = Vector3.Distance(centerPosition, RaycastVive.pointerHitPosition);
-        Debug.Log("Treffer! Time: " + time + "; source: " + source + "; distance: " + distance.ToString("f3"));
+        //Debug.Log("Center: " + center.transform.localPosition);
+        //Debug.Log("Pointer: " + RaycastVive.pointerHitPosition);
+        float distance = Vector2.Distance(centerPosition, RaycastVive.pointerHitPosition)*100f;
+        if(distance < 10)
+        {
+            Debug.Log("Bullseye! ");
+        }
+        else if (distance < 20)
+        {
+            Debug.Log("White 1! ");
+        }
+        else if (distance < 30)
+        {
+            Debug.Log("Red 1! ");
+        }
+        else if (distance < 40)
+        {
+            Debug.Log("White 2! ");
+        }
+        else if (distance <= 50)
+        {
+            Debug.Log("Red 2! ");
+        }
+        Debug.Log("Time: " + time + "; distance: " + distance.ToString("f"));
     }
 
     public void OnPointerDown(MixedRealityPointerEventData eventData) { }
