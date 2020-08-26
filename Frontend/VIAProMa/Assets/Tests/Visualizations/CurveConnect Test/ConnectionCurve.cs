b6bfdb3743176afa61b4836f16cc9431ac35d00c
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-08-26 23:30:08 +0200

    Removed the part that tryed to instantiate photon objetcts with the correct instantiation data

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/ConnectionCurve.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/ConnectionCurve.cs
@@ -112,6 +112,10 @@ public class ConnectionCurve : MonoBehaviour
 
     private void OnDestroy()
     {
+        if (lineController == null)
+        {
+            lineController = GameObject.Find("LineController(Clone)").GetComponent<LineController>();
+        }
         lineController.curves.Remove(this);
     }
 }
