d7d4f959f7c9ac88b471a45d1954575b254ec12d
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-08-27 16:19:21 +0200

    Added a scene for the UserEvaluation of the connection curves

---
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/UserEvaluation/EvaluationTestscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Threading.Tasks;
+
+public class EvaluationTestscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToEvalRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private async void ConnectToEvalRoom()
+    {
+        while (!PhotonNetwork.IsConnected || !PhotonNetwork.InLobby)
+        {
+            await Task.Yield();
+        }
+        Debug.Log("Connecting to room");
+        PhotonNetwork.JoinOrCreateRoom("SebEvalRoom", null, null);
+    }
+
+}
