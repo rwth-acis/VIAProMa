d83e80b7424a061f19ee881f1c2d329e84d20385
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-09-07 15:24:58 +0300

    Cleaned up the code, reorganized methods, fixed a bug where no hit result was being reported because of a delay between the click and the distance masurement

--- Frontend/VIAProMa/Assets/Tests/GazeShareTest/PrecisionTest.cs
+++ Frontend/VIAProMa/Assets/Tests/GazeShareTest/PrecisionTest.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using System.IO;
 
+// JSON serializable class
 [Serializable] public class PrecisionData
 {
     public string result;
@@ -18,82 +16,31 @@ using System.IO;
 
 public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
 {
-    //[SerializeField] private GameObject center;
-    //private float spawnRadius = 10f;
+    // Precision test
     private Vector3 hitPosition;
     private Vector3 centerPosition;
-    private string result;
-    // Timeout
+    private string hitArea;
+    private float distance;
+    private PrecisionData precisionData;
+    private string jsonPrecisionData;
+    // Timer
     private TimeSpan spawnTime;
     private TimeSpan currentTime;
     private TimeSpan timeout;
 
-    private PrecisionData precisionData;
-    private string jsonPrecisionData;
-    //private string filePath = GetFilePath("PrecisionData");
-
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
     {
+        // Write down the time when the target was spawned
         spawnTime = currentTime;
-        //DateTime time = eventData.EventTime;
-        IMixedRealityInputSource source = eventData.InputSource;
-
-        //centerPosition = center.transform.localPosition;
+        // Precision test
         centerPosition = transform.position;
         hitPosition = RaycastVive.pointerHitPosition.Equals(InstantiateArrows.far) ? MixedRealityToolkit.InputSystem.GazeProvider.HitPosition : RaycastVive.pointerHitPosition;
-        //Debug.Log("Center: " + centerPosition);
-        //Debug.Log("Pointer: " + hitPosition);
-
-        //float distance = Vector3.Distance(centerPosition, RaycastVive.pointerHitPosition) * 100f;
-        //float distance = Vector3.Distance(centerPosition, MixedRealityToolkit.InputSystem.GazeProvider.HitPosition) * 100f;
-        float distance = Vector3.Distance(centerPosition, hitPosition) * 100f;
-
-        if (distance < 10)
-        {
-            result = "Bullseye!";
-        }
-        else if (distance < 20)
-        {
-            result = "White inner";
-        }
-        else if (distance < 30)
-        {
-            result = "Red inner";
-        }
-        else if (distance < 40)
-        {
-            result = "White outer";
-        }
-        else if (distance <= 50)
-        {
-            result = "Red outer";
-        }
-
-        //Debug.Log("Time: " + time + "; distance: " + distance.ToString("f"));
-        //Debug.Log(result + " Timer: " + GazeShareTester_Evaluation.elapsedTime + "; distance: " + distance.ToString("f"));
-
-        precisionData = new PrecisionData();
-        precisionData.result = result;
-        precisionData.elapsedTime = GazeShareTester_Evaluation.elapsedTime;
-        precisionData.distance = distance;
-        precisionData.centerPosition = centerPosition;
-        precisionData.hitPosition = hitPosition;
-
-        jsonPrecisionData = JsonUtility.ToJson(precisionData);
-        Debug.Log(jsonPrecisionData);
-        jsonPrecisionData = JsonUtility.ToJson(precisionData, true);
-        WriteString("PrecisionData", jsonPrecisionData);
-
+        distance = Vector3.Distance(centerPosition, hitPosition) * 100f;
+        hitArea = DetermineHitArea();
+        OutputPrecision(hitArea);
+        // Spawn the next target
         MoveTarget(gameObject);
-
-        //TimerWindow.timer.Stop();
     }
-
-    public void OnPointerDown(MixedRealityPointerEventData eventData) { }
-
-    public void OnPointerDragged(MixedRealityPointerEventData eventData) { }
-
-    public void OnPointerUp(MixedRealityPointerEventData eventData) { }
 
     // Start is called before the first frame update
     void Start()
@@ -113,46 +60,90 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         currentTime = GazeShareTester_Evaluation.timer.Elapsed;
         if (currentTime - spawnTime > timeout)
         {
-            precisionData = new PrecisionData();
-            precisionData.result = "TIMEOUT";
-            precisionData.elapsedTime = GazeShareTester_Evaluation.elapsedTime;
-            precisionData.distance = float.NaN;
-            precisionData.centerPosition = centerPosition;
-            precisionData.hitPosition = Vector3.positiveInfinity;
-
-            jsonPrecisionData = JsonUtility.ToJson(precisionData);
-            Debug.Log(jsonPrecisionData);
-            jsonPrecisionData = JsonUtility.ToJson(precisionData, true);
-            WriteString("PrecisionData", jsonPrecisionData);
-
-            spawnTime = currentTime;
+            OutputPrecision("TIMEOUT");
             MoveTarget(gameObject); 
         }
     }
 
     // Moves the target to a new position in the user's field of view and sets its spawn time to current time
-    void MoveTarget(GameObject obj)
+    private void MoveTarget(GameObject obj)
     {
-        //gameObject.SetActive(false);
         obj.gameObject.transform.position = new Vector3(UnityEngine.Random.Range(0.5f, 3f), UnityEngine.Random.Range(0.5f, 3f), 2.5f);
         spawnTime = currentTime;
-        //gameObject.transform.position = UnityEngine.Random.onUnitSphere * spawnRadius;
-        //gameObject.transform.rotation = Quaternion.RotateTowards(Camera.main.transform.rotation, transform.rotation, 360);
-        //gameObject.SetActive(true);
     }
 
+    // Determines which area of the shooting target was hit by the user
+    private string DetermineHitArea()
+    {
+        if (distance < 10)
+        {
+            return "Bullseye!";
+        }
+        else if (distance < 20)
+        {
+            return "White inner";
+        }
+        else if (distance < 30)
+        {
+            return "Red inner";
+        }
+        else if (distance < 40)
+        {
+            return "White outer";
+        }
+        else if (distance <= 50)
+        {
+            return "Red outer";
+        }
+        else
+        {
+            return "TIMEOUT";
+        }
+    } 
+
+    // Creates a precisionData object, fills it with precision information, converts it to a JSON string and saves it in a file 
+    private void OutputPrecision(string result)
+    {
+        precisionData = new PrecisionData();
+        precisionData.elapsedTime = GazeShareTester_Evaluation.elapsedTime;
+        precisionData.centerPosition = centerPosition;
+        if (hitArea == "TIMEOUT")
+        {
+            precisionData.result = "TIMEOUT";
+            precisionData.distance = float.NaN;
+            precisionData.hitPosition = Vector3.positiveInfinity;
+        }
+        else
+        {
+            precisionData.result = hitArea;
+            precisionData.distance = distance;
+            precisionData.hitPosition = hitPosition;
+        }
+
+        jsonPrecisionData = JsonUtility.ToJson(precisionData);
+        Debug.Log(jsonPrecisionData);
+        jsonPrecisionData = JsonUtility.ToJson(precisionData, true);
+        WriteString("PrecisionData", jsonPrecisionData);
+    }
+
+    // Retrieves the file path for the provided file name
     private static string GetFilePath(string fileName)
     {
         return Path.Combine(Application.streamingAssetsPath, fileName);
     }
 
-    static void WriteString(string fileName, string line)
+    // Opens the provided file and appends the provided string to it
+    static void WriteString(string fileName, string input)
     {
         string filePath = GetFilePath(fileName);
-
-        //Write some text to the test.txt file
         StreamWriter writer = new StreamWriter(filePath, true);
-        writer.WriteLine(line);
+        writer.WriteLine(input);
         writer.Close();
     }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData) { }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData) { }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData) { }
 }
