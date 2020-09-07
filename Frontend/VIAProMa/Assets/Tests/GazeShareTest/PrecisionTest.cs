70c5b85ef26081ccaf79d5113f364714adee0cfe
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-09-07 14:05:59 +0300

    Set up precision data export to a file (for Universal Windows Platform), modified the JSON string, hid unnecessary scene objects

--- Frontend/VIAProMa/Assets/Tests/GazeShareTest/PrecisionTest.cs
+++ Frontend/VIAProMa/Assets/Tests/GazeShareTest/PrecisionTest.cs
@@ -3,13 +3,15 @@ using Microsoft.MixedReality.Toolkit.Input;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 [Serializable] public class PrecisionData
 {
     public string result;
-    public TimeSpan elapsedTime;
-    public float distance;
+    public string elapsedTime;
+    public string distance;
     public Vector3 centerPosition;
     public Vector3 hitPosition;
 }
@@ -28,6 +30,7 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
 
     private PrecisionData precisionData;
     private string jsonPrecisionData;
+    //private string filePath = GetFilePath("PrecisionData");
 
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
     {
@@ -38,8 +41,9 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         //centerPosition = center.transform.localPosition;
         centerPosition = transform.position;
         hitPosition = RaycastVive.pointerHitPosition.Equals(InstantiateArrows.far) ? MixedRealityToolkit.InputSystem.GazeProvider.HitPosition : RaycastVive.pointerHitPosition;
-        Debug.Log("Center: " + centerPosition);
-        Debug.Log("Pointer: " + hitPosition);
+        //Debug.Log("Center: " + centerPosition);
+        //Debug.Log("Pointer: " + hitPosition);
+
         //float distance = Vector3.Distance(centerPosition, RaycastVive.pointerHitPosition) * 100f;
         //float distance = Vector3.Distance(centerPosition, MixedRealityToolkit.InputSystem.GazeProvider.HitPosition) * 100f;
         float distance = Vector3.Distance(centerPosition, hitPosition) * 100f;
@@ -50,31 +54,35 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         }
         else if (distance < 20)
         {
-            result = "White inner.";
+            result = "White inner";
         }
         else if (distance < 30)
         {
-            result = "Red inner.";
+            result = "Red inner";
         }
         else if (distance < 40)
         {
-            result = "White outer...";
+            result = "White outer";
         }
         else if (distance <= 50)
         {
-            result = "Red outer...";
+            result = "Red outer";
         }
 
         //Debug.Log("Time: " + time + "; distance: " + distance.ToString("f"));
-        Debug.Log(result + " Timer: " + GazeShareTester_Evaluation.elapsedTime + "; distance: " + distance.ToString("f"));
+        //Debug.Log(result + " Timer: " + GazeShareTester_Evaluation.elapsedTime + "; distance: " + distance.ToString("f"));
 
         precisionData = new PrecisionData();
         precisionData.result = result;
-        precisionData.elapsedTime = GazeShareTester_Evaluation.timer.Elapsed;
-        precisionData.distance = distance;
+        precisionData.elapsedTime = GazeShareTester_Evaluation.elapsedTime;
+        precisionData.distance = distance.ToString("f");
         precisionData.centerPosition = centerPosition;
         precisionData.hitPosition = hitPosition;
+
         jsonPrecisionData = JsonUtility.ToJson(precisionData);
+        Debug.Log(jsonPrecisionData);
+        jsonPrecisionData = JsonUtility.ToJson(precisionData, true);
+        WriteString("PrecisionData", jsonPrecisionData);
 
         MoveTarget(gameObject);
 
@@ -92,9 +100,11 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
     {
         hitPosition = InstantiateArrows.far;
         centerPosition = transform.position;
-
+        // Timer
         spawnTime = GazeShareTester_Evaluation.timer.Elapsed;
         timeout = TimeSpan.FromSeconds(5.0);
+        // Precision data
+        WriteString("PrecisionData", "NEW TEST " + DateTime.Now);
     }
 
     // Update is called once per frame
@@ -104,6 +114,16 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         if (currentTime - spawnTime > timeout)
         {
             precisionData = new PrecisionData();
+            precisionData.result = "TIMEOUT";
+            precisionData.elapsedTime = GazeShareTester_Evaluation.elapsedTime;
+            precisionData.distance = float.NaN.ToString();
+            precisionData.centerPosition = centerPosition;
+            precisionData.hitPosition = Vector3.positiveInfinity;
+
+            jsonPrecisionData = JsonUtility.ToJson(precisionData);
+            Debug.Log(jsonPrecisionData);
+            jsonPrecisionData = JsonUtility.ToJson(precisionData, true);
+            WriteString("PrecisionData", jsonPrecisionData);
 
             spawnTime = currentTime;
             MoveTarget(gameObject); 
@@ -119,5 +139,20 @@ public class PrecisionTest : MonoBehaviour, IMixedRealityPointerHandler
         //gameObject.transform.position = UnityEngine.Random.onUnitSphere * spawnRadius;
         //gameObject.transform.rotation = Quaternion.RotateTowards(Camera.main.transform.rotation, transform.rotation, 360);
         //gameObject.SetActive(true);
+    }
+
+    private static string GetFilePath(string fileName)
+    {
+        return Path.Combine(Application.streamingAssetsPath, fileName);
+    }
+
+    static void WriteString(string fileName, string line)
+    {
+        string filePath = GetFilePath(fileName);
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(filePath, true);
+        writer.WriteLine(line);
+        writer.Close();
     }
 }
