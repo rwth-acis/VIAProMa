2dfe61f91af271767cb741f531a7b2138db66c1a
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-09-10 14:19:42 +0200

    fixed a bug, where the curve update stoped under strange race conditions

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/JoinedCurveGeneration.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/JoinedCurveGeneration.cs
@@ -10,52 +10,70 @@ public class JoinedCurveGeneration : MonoBehaviour
 
     public static async Task UpdateAsync(List<ConnectionCurve> curves, float stepSize)
     {
-        while (true)
+        try
         {
-            var tasks = new Dictionary<Task<Vector3[]>, ConnectionCurve>();
-            foreach (ConnectionCurve connectionCurve in curves)
-            {    
-                tasks.Add(JoinedCurve(connectionCurve,stepSize),connectionCurve);
-            }
-            while (tasks.Count > 0)
+            while (true)
             {
-                Task<Vector3[]> finishedTask = await Task.WhenAny(tasks.Keys);
-                ConnectionCurve connectionCurve = tasks[finishedTask];
-                connectionCurve.lineRenderer.positionCount = finishedTask.Result.Length;
-                connectionCurve.lineRenderer.SetPositions(finishedTask.Result); 
-                tasks.Remove(finishedTask);
+                var tasks = new Dictionary<Task<Vector3[]>, ConnectionCurve>();
+                foreach (ConnectionCurve connectionCurve in curves)
+                {
+                    tasks.Add(JoinedCurve(connectionCurve, stepSize), connectionCurve);
+                }
+                while (tasks.Count > 0)
+                {
+                    Task<Vector3[]> finishedTask = await Task.WhenAny(tasks.Keys);
+                    ConnectionCurve connectionCurve = tasks[finishedTask];
+                    //connectionCurve can somehow be null here
+                    if (connectionCurve != null)
+                    {
+                        connectionCurve.lineRenderer.positionCount = finishedTask.Result.Length;
+                        connectionCurve?.lineRenderer.SetPositions(finishedTask.Result);
+                    }
+                    tasks.Remove(finishedTask);
+                }
+                await Task.Yield();
             }
-            await Task.Yield();
+        }
+        catch (Exception e)
+        {
+            throw e;
         }
     }
 
     static async Task<Vector3[]> JoinedCurve(ConnectionCurve connectionCurve, float stepSize, int segmentCount = 60)
     {
-        Vector3[] curve = SimpleCurveGerneration.StartGeneration(connectionCurve.start, connectionCurve.goal, segmentCount);
-        if (CurveGenerator.CurveCollsionCheck(curve, connectionCurve.start, connectionCurve.goal))
+        try
         {
-            IntTriple startCell = IntTriple.VectorToCell(connectionCurve.start.transform.position, stepSize);
-            IntTriple goalCell = IntTriple.VectorToCell(connectionCurve.goal.transform.position, stepSize);
-            Task<GridSearch.SearchResult<IntTriple>> astarTask = AStar.AStarGridSearchAsync(startCell, goalCell, stepSize, connectionCurve.start, connectionCurve.goal);
-            await astarTask;
-            if (astarTask.Result.path == null)
+            Vector3[] curve = SimpleCurveGerneration.StartGeneration(connectionCurve.start, connectionCurve.goal, segmentCount);
+            if (CurveGenerator.CurveCollsionCheck(curve, connectionCurve.start, connectionCurve.goal))
             {
-                Task<GridSearch.SearchResult<IntTriple>> greedyTask = Greedy.GreedyGridSearchAsync(startCell, goalCell, stepSize, connectionCurve.start, connectionCurve.goal);
-                await greedyTask;
-                if (greedyTask.Result.path == null)
+                IntTriple startCell = IntTriple.VectorToCell(connectionCurve.start.transform.position, stepSize);
+                IntTriple goalCell = IntTriple.VectorToCell(connectionCurve.goal.transform.position, stepSize);
+                Task<GridSearch.SearchResult<IntTriple>> astarTask = AStar.AStarGridSearchAsync(startCell, goalCell, stepSize, connectionCurve.start, connectionCurve.goal);
+                await astarTask;
+                if (astarTask.Result.path == null)
                 {
-                    curve = SimpleCurveGerneration.StandartCurve(connectionCurve.start.transform.position, connectionCurve.goal.transform.position, segmentCount, 0.5f);
+                    Task<GridSearch.SearchResult<IntTriple>> greedyTask = Greedy.GreedyGridSearchAsync(startCell, goalCell, stepSize, connectionCurve.start, connectionCurve.goal);
+                    await greedyTask;
+                    if (greedyTask.Result.path == null)
+                    {
+                        curve = SimpleCurveGerneration.StandartCurve(connectionCurve.start.transform.position, connectionCurve.goal.transform.position, segmentCount, 0.5f);
+                    }
+                    else
+                    {
+                        curve = CurveGenerator.IntTripleArrayToCurve(greedyTask.Result.path, connectionCurve.start.transform.position, connectionCurve.goal.transform.position, stepSize);
+                    }
                 }
                 else
                 {
-                    curve = CurveGenerator.IntTripleArrayToCurve(greedyTask.Result.path, connectionCurve.start.transform.position, connectionCurve.goal.transform.position, stepSize);
+                    curve = CurveGenerator.IntTripleArrayToCurve(astarTask.Result.path, connectionCurve.start.transform.position, connectionCurve.goal.transform.position, stepSize);
                 }
             }
-            else
-            {
-                curve = CurveGenerator.IntTripleArrayToCurve(astarTask.Result.path,connectionCurve.start.transform.position, connectionCurve.goal.transform.position,stepSize);
-            }
+            return curve;
         }
-        return curve;
+        catch (Exception e)
+        {
+            throw e;
+        }
     }
 }
