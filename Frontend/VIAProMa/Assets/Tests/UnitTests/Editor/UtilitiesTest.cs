df5c160f2ae5a5b4954c3cfd12144052d4b00260
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2020-10-20 21:21:29 +0200

    Added a play mode unit tetsts folder, moved the existing edit mode tests folder to the tests directory, added Photon assembly references to fix more errors

---
+++ Frontend/VIAProMa/Assets/Tests/UnitTests/Editor/UtilitiesTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class UtilitiesTest
+    {
+        /// <summary>
+        /// Tests the Utility function which removes special characters from strings
+        /// </summary>
+        [Test]
+        public void UtilitiesTestSimplePasses()
+        {
+            const string input = "Hello\\-+^ World~/.,;:";
+            const string expected = "Hello World";
+            string result = StringUtilities.RemoveSpecialCharacters(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
