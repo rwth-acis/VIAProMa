b3d6ead49d7a5f5c102d3c595f37daec757479ee
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-10-23 18:55:39 +0200

    Added summarys

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/UserEvaluation/TaskStarter.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/UserEvaluation/TaskStarter.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Delets all Connection Curves and then creates a Connection Curve for each pair in the startObjects and goalObjects list, once the StartTask method gets called. Only used for test purposes.
+/// </summary>
 public class TaskStarter : MonoBehaviour
 {
-    public List<GameObject> startObject;
-    public List<GameObject> goalObject;
+    public List<GameObject> startObjects;
+    public List<GameObject> goalObjects;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Delets all Connection Curves and then creates a Connection Curve for each pair in the startObjects and goalObjects list.
+    /// </summary>
     public void StartTask()
     {
         ConnectionCurveManager controller = GameObject.FindObjectOfType<ConnectionCurveManager>();
         controller.DeleteAllCurves();
-        for (int i = 0; i < startObject.Count; i++)
+        for (int i = 0; i < startObjects.Count; i++)
         {
-            controller.CreateConnectionCurveScene(startObject[i],goalObject[i]);
+            controller.CreateConnectionCurveScene(startObjects[i],goalObjects[i]);
         }
     }
 }
