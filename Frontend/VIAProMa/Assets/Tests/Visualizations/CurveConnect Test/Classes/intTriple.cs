e7e0ce5d361c7b76b472c3f06378dcb9722f07ca
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-10-26 00:17:56 +0100

    Added summarys

--- Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/Classes/intTriple.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/CurveConnect Test/Classes/intTriple.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A triple of type int. Contains functions to use it to store and manage cells for a 3D grid.
+/// </summary>
 public class IntTriple
 {
-    //public Triple<int, int, int> triple;
     public int x;
     public int y;
     public int z;
@@ -62,13 +64,17 @@ public class IntTriple
         return new IntTriple(triple.x / scalar, triple.y / scalar, triple.z / scalar);
     }
 
-    //All conversion between cells(IntTriples) and other search related types
+    /// <summary>
+    /// All conversion between cells(IntTriples) and other search related types
+    /// </summary>
     public static Vector3 CellToVector(IntTriple triple, float stepSize)
     {
         return new Vector3(triple.x, triple.y, triple.z) *stepSize + new Vector3(stepSize/2,stepSize/2,stepSize/2);
     }
 
-    //Calculates the coorosponding cell on the grid
+    /// <summary>
+    /// Calculates the corresponding cell on the grid
+    /// </summary>
     public static IntTriple VectorToCell(Vector3 vector, float stepSize)
     {
         Vector3 inverseVector = vector /stepSize;
@@ -76,27 +82,5 @@ public class IntTriple
         return new IntTriple(   inverseVector.x >= 0 ? (int)inverseVector.x : Mathf.FloorToInt(inverseVector.x),
                                 inverseVector.y >= 0 ? (int)inverseVector.y : Mathf.FloorToInt(inverseVector.y),
                                 inverseVector.z >= 0 ? (int)inverseVector.z : Mathf.FloorToInt(inverseVector.z));
-    }
-
-    public static IntTriple CellToCluster(IntTriple cell, int clusterSize)
-    {
-        //This is necassary, because the clustergrid starts at 0 in the positiv and at -1 in the negativ
-        IntTriple newCell = new IntTriple(cell);
-        if (newCell.x < 0)
-            newCell.x -= clusterSize - 1;
-        if (newCell.y < 0)
-            newCell.y -= clusterSize - 1;
-        if (newCell.z < 0)
-            newCell.z -= clusterSize - 1;
-        return newCell / clusterSize;
-    }
-
-    //Iterates through the triples like (-1,0,0) -> (1,0,0) -> (0,-1,0) -> (0,1,0) ...
-    public static IntTriple TripleIterator(IntTriple triple)
-    {
-        if (triple.x < 0 || triple.y < 0 || triple.z < 0)
-            return (triple * -1);
-        else
-            return new IntTriple(0, -triple.x, -triple.y);
     }
 }
