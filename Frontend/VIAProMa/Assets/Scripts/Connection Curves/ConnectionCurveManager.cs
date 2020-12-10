8db4791169bdd0032b39cacf2a51ceaaae71b06b
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2020-12-10 21:27:58 +0100

    Changed the allocation strategy for the nativ arrays for a massiv performance boost

--- Frontend/VIAProMa/Assets/Scripts/Connection Curves/ConnectionCurveManager.cs
+++ Frontend/VIAProMa/Assets/Scripts/Connection Curves/ConnectionCurveManager.cs
@@ -7,6 +7,7 @@ using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using System.Threading.Tasks;
 using HoloToolkit.Unity;
+using Unity.Collections;
 
 /// <summary>
 /// Manages the connect and disconnect process of Connection Curves and provides functions to manage curves.
