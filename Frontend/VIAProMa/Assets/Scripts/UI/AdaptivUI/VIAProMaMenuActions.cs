fbe5393077fa5e50e0eb4cf0f83df3a9a2226e51
SebastianMeinberger <sebastian.meinberger@rwth-aachen.de>
2021-01-02 13:43:37 +0100

    Added the remove function and a fuction to get visualisations from an input source

--- Frontend/VIAProMa/Assets/Scripts/UI/AdaptivUI/VIAProMaMenuActions.cs
+++ Frontend/VIAProMa/Assets/Scripts/UI/AdaptivUI/VIAProMaMenuActions.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using HoloToolkit.Unity;
+using Photon.Pun;
 public class VIAProMaMenuActions : MonoBehaviour
 {
     public void TestAction(VirtualToolEventData data)
@@ -27,5 +28,47 @@ public class VIAProMaMenuActions : MonoBehaviour
         {
             target.transform.root.localScale /= 2;
         }
+    }
+
+    public void Remove(BaseInputEventData eventData)
+    {
+        GameObject target = GetVisualisationFromInputSource(eventData.InputSource);
+        if (target == null)
+        {
+            return;
+        }
+        if (target.GetComponentInChildren<PhotonView>() != null)
+        {
+            PhotonNetwork.Destroy(target);
+        }
+        else
+        {
+            Destroy(target);
+        }
+    }
+
+    GameObject GetVisualisationFromInputSource(IMixedRealityInputSource source)
+    {
+        foreach (var pointer in source.Pointers)
+        {
+            GameObject target = pointer.Result?.CurrentPointerTarget;
+            if (target != null)
+            {
+                while (target != null && !IsVisualisation(target))
+                {
+                    target = target.transform.parent?.gameObject;
+                }
+                if (target != null && IsVisualisation(target))
+                {
+                    return target;
+                }
+            }
+        }
+        return null;
+    }
+
+    bool IsVisualisation(GameObject gameObject)
+    {
+        return gameObject.GetComponent<Visualization>() != null;
     }
 }
