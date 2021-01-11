173cf02121d89e64c234c29d62e7a903654a3e1b
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2021-01-11 14:07:38 +0100

    Created three more tests for HorizontalObjectArray with multiple objects

--- Frontend/VIAProMa/Assets/Tests/UnitTests/PlayMode/UtilitiesPlayModeTests.cs
+++ Frontend/VIAProMa/Assets/Tests/UnitTests/PlayMode/UtilitiesPlayModeTests.cs
@@ -24,10 +24,16 @@ namespace Tests
 
         private RandomColor randomColor;
 
+        private GameObject arrayObject;
+        private HorizontalObjectArray horizontalObjectArray;
         private GameObject arrayObject1;
-        //private GameObject arrayObject2;
+        private GameObject arrayObject2;
+        private GameObject arrayObject3;
+        private GameObject arrayObject4;
         private HorizontalObjectArray horizontalObjectArray1;
-        //private HorizontalObjectArray horizontalObjectArray2;
+        private HorizontalObjectArray horizontalObjectArray2;
+        private HorizontalObjectArray horizontalObjectArray3;
+        private HorizontalObjectArray horizontalObjectArray4;
 
         [SetUp]
         public void Setup()
@@ -46,13 +52,21 @@ namespace Tests
 
             testObject.AddComponent<MeshRenderer>();
 
+            arrayObject = GameObject.Instantiate(new GameObject());
+            arrayObject = GameObject.Instantiate(new GameObject());
+            horizontalObjectArray = arrayObject.AddComponent<HorizontalObjectArray>();
             arrayObject1 = GameObject.Instantiate(new GameObject());
-            //arrayObject2 = GameObject.Instantiate(new GameObject());
+            arrayObject2 = GameObject.Instantiate(new GameObject());
+            arrayObject3 = GameObject.Instantiate(new GameObject());
+            arrayObject4 = GameObject.Instantiate(new GameObject());
             arrayObject1.AddComponent<BoundingBox>();
-            //arrayObject2.AddComponent<BoundingBox>();
+            arrayObject2.AddComponent<BoundingBox>();
+            arrayObject3.AddComponent<BoundingBox>();
+            arrayObject4.AddComponent<BoundingBox>();
             horizontalObjectArray1 = arrayObject1.AddComponent<HorizontalObjectArray>();
-            //horizontalObjectArray2 = arrayObject2.AddComponent<HorizontalObjectArray>();
-
+            horizontalObjectArray2 = arrayObject2.AddComponent<HorizontalObjectArray>();
+            horizontalObjectArray3 = arrayObject3.AddComponent<HorizontalObjectArray>();
+            horizontalObjectArray4 = arrayObject4.AddComponent<HorizontalObjectArray>();
         }
 
         [UnityTest]
@@ -134,12 +148,83 @@ namespace Tests
 
             // Use the Assert class to test conditions.
             //Debug.Log("Expected: " + initialPosition.y + "; actual: " + finalPosition.y);
-            Assert.AreEqual(initialPosition, finalPosition);
+            Assert.AreEqual(initialPosition, finalPosition); // If only one object is passed, its coordinates do not change
+        }
+
+        [UnityTest]
+        public IEnumerator HorizontalObjectArray_MultipleObjectsOdd_CentalObjectAsArray() // Any other object from the array would not work
+        {
+            // Arrange
+            Vector3 initialPosition1 = arrayObject1.transform.position;
+            Vector3 initialPosition2 = arrayObject2.transform.position;
+            Vector3 initialPosition3 = arrayObject3.transform.position;
+            horizontalObjectArray2.Collection = new GameObject[] { arrayObject1, arrayObject2, arrayObject3 };
+
+            // Use yield to skip a frame.
+            yield return new WaitForSeconds(Time.deltaTime);
+            Vector3 finalPosition1 = arrayObject1.transform.position;
+            Vector3 finalPosition2 = arrayObject2.transform.position;
+            Vector3 finalPosition3 = arrayObject3.transform.position;
+
+            // Use the Assert class to test conditions.
+            Assert.AreNotEqual(initialPosition1, finalPosition1);
+            Assert.AreEqual(initialPosition2, finalPosition2);
+            Assert.AreNotEqual(initialPosition3, finalPosition3);
+            Assert.AreEqual(finalPosition1.x, -finalPosition3.x);
+            //Assert.IsTrue(initialPosition1 == finalPosition1 && initialPosition2 == finalPosition2);
+        }
+
+        [UnityTest]
+        public IEnumerator HorizontalObjectArray_MultipleObjectsOdd()
+        {
+            // Arrange
+            Vector3 initialPosition1 = arrayObject1.transform.position;
+            Vector3 initialPosition2 = arrayObject2.transform.position;
+            Vector3 initialPosition3 = arrayObject3.transform.position;
+            horizontalObjectArray.Collection = new GameObject[] { arrayObject1, arrayObject2, arrayObject3 };
+
+            // Use yield to skip a frame.
+            yield return new WaitForSeconds(Time.deltaTime);
+            Vector3 finalPosition1 = arrayObject1.transform.position;
+            Vector3 finalPosition2 = arrayObject2.transform.position;
+            Vector3 finalPosition3 = arrayObject3.transform.position;
+
+            // Use the Assert class to test conditions.
+            Assert.AreNotEqual(initialPosition1, finalPosition1);
+            Assert.AreEqual(initialPosition2, finalPosition2);
+            Assert.AreNotEqual(initialPosition3, finalPosition3);
+            Assert.AreEqual(finalPosition1.x, -finalPosition3.x);
+        }
+
+        [UnityTest]
+        public IEnumerator HorizontalObjectArray_MultipleObjectsEven()
+        {
+            // Arrange
+            Vector3 initialPosition1 = arrayObject1.transform.position;
+            Vector3 initialPosition2 = arrayObject2.transform.position;
+            Vector3 initialPosition3 = arrayObject3.transform.position;
+            Vector3 initialPosition4 = arrayObject4.transform.position;
+            horizontalObjectArray.Collection = new GameObject[] { arrayObject1, arrayObject2, arrayObject3, arrayObject4 };
+
+            // Use yield to skip a frame.
+            yield return new WaitForSeconds(Time.deltaTime);
+            Vector3 finalPosition1 = arrayObject1.transform.position;
+            Vector3 finalPosition2 = arrayObject2.transform.position;
+            Vector3 finalPosition3 = arrayObject3.transform.position;
+            Vector3 finalPosition4 = arrayObject4.transform.position;
+
+            // Use the Assert class to test conditions.
+            Assert.AreNotEqual(initialPosition1, finalPosition1);
+            Assert.AreNotEqual(initialPosition2, finalPosition2);
+            Assert.AreNotEqual(initialPosition3, finalPosition3);
+            Assert.AreNotEqual(initialPosition4, finalPosition4);
+            Assert.AreEqual(finalPosition1.x, -finalPosition4.x);
+            Assert.AreEqual(finalPosition2.x, -finalPosition3.x);
         }
 
         /* Template
         [UnityTest]
-        public IEnumerator HorizontalObjectArray_SingleObject()
+        public IEnumerator ScriptName_FunctionName_TestName()
         {
             // Arrange
 
@@ -155,6 +240,10 @@ namespace Tests
         {
             GameObject.Destroy(testObject);
             GameObject.Destroy(targetObject);
+            GameObject.Destroy(arrayObject);
+            GameObject.Destroy(arrayObject1);
+            GameObject.Destroy(arrayObject2);
+            GameObject.Destroy(arrayObject3);
         }
     }
 }
