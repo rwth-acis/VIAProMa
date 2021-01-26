ec65c446dae6365180dbe9c5cae9fe5bee97ec5c
EmilieHK <emilie.hastrup.kiil@rwth-aachen.de>
2021-01-26 17:28:06 +0100

    Started working on GitHub Oicd Provider

---
+++ Frontend/VIAProMa/Assets/i5 Toolkit for Unity/Samples/OpenID Connect/GitHubBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using i5.Toolkit.Core.OpenIDConnectClient;
+using i5.Toolkit.Core.ServiceCore;
+
+namespace i5.Toolkit.Core.Examples.OpenIDConnectClient
+{
+    /// <summary>
+    /// Bootstrapper for initializing the OpenID Connect service for the github provider
+    /// </summary>
+    public class GitHubBootstrapper : BaseServiceBootstrapper
+    {
+        protected override void RegisterServices()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            oidc.OidcProvider = new GitHubOidcProvider();
+
+            oidc.RedirectURI = "i5:/";
+            ServiceManager.RegisterService(oidc);
+        }
+
+        protected override void UnRegisterServices()
+        {
+            ServiceManager.RemoveService<OpenIDConnectService>();
+        }
+    }
+}
