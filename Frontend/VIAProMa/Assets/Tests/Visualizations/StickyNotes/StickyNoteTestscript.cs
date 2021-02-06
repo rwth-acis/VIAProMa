744e75b56e65b1c11d39b02aab19841e40ec3daf
Carlos Classen <carlos.classen@rwth-aachen.de>
2021-02-06 20:10:49 +0100

    [EDITED] Testscene-Notes

--- Frontend/VIAProMa/Assets/Tests/Visualizations/StickyNotes/StickyNoteTestscript.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/StickyNotes/StickyNoteTestscript.cs
@@ -1,57 +1,79 @@
-﻿using i5.VIAProMa.Visualizations.Common;
-using i5.VIAProMa.Visualizations.Common.Data.DataSets;
+﻿using i5.VIAProMa.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using i5.VIAProMa.UI.InputFields;
 
-namespace i5.VIAProMa.Visualizations.Haftnotizen{
 
-    public class StickyNoteTestscript : MonoBehaviour
+
+
+public class StickyNoteTestscript : MonoBehaviour
+{   
+    // References to the Note Instance and its components
+    [Header("Sticky Note Instance")]
+    [SerializeField] private GameObject noteInstance;
+    [Header("Sticky Note Components")]
+    [SerializeField] private Interactable pinButton;
+    [SerializeField] private Interactable unpinButton;
+    [SerializeField] private Interactable colorButton;
+    [SerializeField] private Interactable editButton;
+    [SerializeField] private Interactable keyboardAcceptButton;
+    [SerializeField] private Interactable clearButton;
+
+    // Helper variables
+    private bool pinned = false;
+    private bool write = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
     {   
-        // References to the Note Instance and its components
-        [Header("Sticky Note Instance")]
-        [SerializeField] private GameObject noteInstance;
-        [Header("Sticky Note Components")]
-        [SerializeField] private GameObject pinButton;
-        [SerializeField] private GameObject unpinButton;
-        [SerializeField] private GameObject colorButton;
-        [SerializeField] private GameObject editButton;
-        [SerializeField] private GameObject clearButton;
-        [SerializeField] private InputField inputField;
-        [SerializeField] private HaftnotizenVisualController controller;
-
-        // Start is called before the first frame update
-        void Start()
+        // Pin/Unpin Note
+        if (Input.GetKeyDown(KeyCode.F5))
         {
-        
+            if (!pinned)
+            {
+                pinButton.TriggerOnClick();
+                pinned = true;
+            }
+            else
+            {
+                unpinButton.TriggerOnClick();
+                pinned = false;
+            }
+            
         }
-
-        // Update is called once per frame
-        void Update()
-        {   
-            // Pin/Unpin Note
-            if (Input.GetKeyDown(KeyCode.F5))
+        // Cycle Color
+        if (Input.GetKeyDown(KeyCode.F6))
+        {
+            colorButton.TriggerOnClick();
+        }
+        // Edit Note
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            if (!write)
             {
-                
+                editButton.TriggerOnClick();
+                write = true;
             }
-            // Cycle Color
-            if (Input.GetKeyDown(KeyCode.F6))
+            else
             {
-                controller.colorCycler.cycle();
+                keyboardAcceptButton.TriggerOnClick();
+                write = false;
             }
-            // Edit Note
-            if (Input.GetKeyDown(KeyCode.F7))
-            {
-                inputField.OnClick();
-            }
-            // Clear note
-            if (Input.GetKeyDown(KeyCode.F8))
-            {
-                controller.inputField.Text = "";
-            }
+        }
+        // Clear note
+        if (Input.GetKeyDown(KeyCode.F8))
+        {
+           clearButton.TriggerOnClick();
         }
     }
 }
+
 
     
