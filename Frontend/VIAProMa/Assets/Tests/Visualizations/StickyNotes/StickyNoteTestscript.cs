622255629566fcaa7108703c1beaf678265d7a0d
Carlos Classen <carlos.classen@rwth-aachen.de>
2021-02-06 19:43:09 +0100

    [EDITED] Testscene-Notes

--- Frontend/VIAProMa/Assets/Tests/Visualizations/StickyNotes/StickyNoteTestscript.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/StickyNotes/StickyNoteTestscript.cs
@@ -3,42 +3,53 @@ using i5.VIAProMa.Visualizations.Common.Data.DataSets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using i5.VIAProMa.UI.InputFields;
 
-public class StickyNoteTestscript : MonoBehaviour
-{   
-    // Reference to Note Prefab
-    [SerializeField] private GameObject notePrefab;
-    // Variable to hold the created note instance
-    private GameObject noteInstance;
+namespace i5.VIAProMa.Visualizations.Haftnotizen{
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        noteInstance = Instantiate(notePrefab, new Vector3(0,0,2), new Quaternion(0, -12, 0, 100));
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class StickyNoteTestscript : MonoBehaviour
     {   
-        // Pin/Unpin Note
-        if (Input.GetKeyDown(KeyCode.F5))
+        // References to the Note Instance and its components
+        [Header("Sticky Note Instance")]
+        [SerializeField] private GameObject noteInstance;
+        [Header("Sticky Note Components")]
+        [SerializeField] private GameObject pinButton;
+        [SerializeField] private GameObject unpinButton;
+        [SerializeField] private GameObject colorButton;
+        [SerializeField] private GameObject editButton;
+        [SerializeField] private GameObject clearButton;
+        [SerializeField] private InputField inputField;
+        [SerializeField] private HaftnotizenVisualController controller;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            
+        
         }
-        // Cycle Color
-        if (Input.GetKeyDown(KeyCode.F6))
-        {
-            
-        }
-        // Edit Note
-        if (Input.GetKeyDown(KeyCode.F7))
-        {
-            
-        }
-        // Clear note
-        if (Input.GetKeyDown(KeyCode.F8))
-        {
-            
+
+        // Update is called once per frame
+        void Update()
+        {   
+            // Pin/Unpin Note
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                
+            }
+            // Cycle Color
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                controller.colorCycler.cycle();
+            }
+            // Edit Note
+            if (Input.GetKeyDown(KeyCode.F7))
+            {
+                inputField.OnClick();
+            }
+            // Clear note
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                controller.inputField.Text = "";
+            }
         }
     }
 }
