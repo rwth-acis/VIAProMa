7777f50e4acf0662e9503b44a216b3d4fa7bfde4
Emilie Hastrup-Kiil <emilie.hastrup.kiil@rwth-aachen.de>
2021-04-05 19:58:02 +0200

    Insterted CreateIssue Button into the Shelf structure

--- Frontend/VIAProMa/Assets/ReqBazaarConnection/CreateIssueMenu.cs
+++ Frontend/VIAProMa/Assets/ReqBazaarConnection/CreateIssueMenu.cs
@@ -1,35 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Org.Requirements_Bazaar.API;
 using Org.Requirements_Bazaar.DataModel;
 using i5.VIAProMa.Shelves.IssueShelf;
-using i5.VIAProMa.DataModel.API;
-using System.Threading.Tasks;
 using TMPro;
 
 public class CreateIssueMenu : MonoBehaviour
 {
-    [SerializeField] private ShelfConfigurationMenu configurationMenu;
+    private ShelfConfigurationMenu configurationMenu;
+    private IssuesLoader issueLoader;
+
+    [SerializeField] private CreateIssueMenuOpener opener;
     [SerializeField] private TextMeshPro issueName;
     [SerializeField] private TextMeshPro issueDescription;
 
     public void Start()
     {
         configurationMenu = GameObject.FindObjectOfType<ShelfConfigurationMenu>();
+        issueLoader = GameObject.FindObjectOfType<IssuesLoader>();
     }
 
-    public void Open()
-    {
-        gameObject.SetActive(true);
-    }
-
-    public void Close()
-    {
-        gameObject.SetActive(false);
-    }
-
-    // Called when the createissue button on the createissue menu is pressed
+    // Called when the createIssue button on the createIssue menu is pressed
     public async void CreateIssue()
     {
         ReqBazShelfConfiguration reqBazShelfConfiguration = (ReqBazShelfConfiguration)configurationMenu.ShelfConfiguration;
@@ -41,6 +31,7 @@ public class CreateIssueMenu : MonoBehaviour
 
         await RequirementsBazaarManager.CreateRequirement(reqBazShelfConfiguration.SelectedProject.id, issueName.text, issueDescription.text, categoryarray);
 
-        Close();
+        issueLoader.LoadContent();
+        opener.CloseMenu();
     }
 }
---
+++ Frontend/VIAProMa/Assets/ReqBazaarConnection/CreateIssueMenuOpener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using i5.Toolkit.Core.ServiceCore;
+using i5.Toolkit.Core.OpenIDConnectClient;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class CreateIssueMenuOpener : MonoBehaviour
+{
+    [SerializeField] CreateIssueMenu createIssueMenu;
+    bool isOpen = false;
+
+
+    public void CloseMenu()
+    {
+        createIssueMenu.gameObject.SetActive(false);
+        isOpen = false;
+        Debug.Log("Try to close menu");
+    }
+
+    public void OpenMenu()
+    {
+        createIssueMenu.gameObject.SetActive(true);
+        isOpen = true;
+        Debug.Log("Try to open menu");
+    }
+
+    public void OpenCreateIssueMenu()
+    {
+        if(createIssueMenu != null)
+        {
+            if (isOpen)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+    }
+}
