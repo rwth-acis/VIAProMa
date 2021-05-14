395c1b8464c9b42a6f5c53c44c20e4630398f149
Julian Staab <julian.staab@rwth-aachen.de>
2021-05-14 10:29:55 +0200

    Updated required files to i5 Toolkit version 1.6.1

--- Frontend/VIAProMa/Assets/i5 Toolkit for Unity/Runtime/Utilities/Rest Connectors/IRestConnector.cs
+++ Frontend/VIAProMa/Assets/i5 Toolkit for Unity/Runtime/Utilities/Rest Connectors/IRestConnector.cs
@@ -7,12 +7,14 @@ namespace i5.Toolkit.Core.Utilities
 {
     public interface IRestConnector
     {
-        Task<WebResponse<string>> GetAsync(string uri);
+        Task<WebResponse<string>> GetAsync(string uri, Dictionary<string, string> headers = null);
 
-        Task<WebResponse<string>> PostAsync(string uri, string postData);
+        Task<WebResponse<string>> PostAsync(string uri, string postData, Dictionary<string, string> headers = null);
 
-        Task<WebResponse<string>> PutAsync(string uri, string postData);
+        Task<WebResponse<string>> PostAsync(string uri, byte[] postData, Dictionary<string, string> headers = null);
 
-        Task<WebResponse<string>> DeleteAsync(string uri);
+        Task<WebResponse<string>> PutAsync(string uri, string postData, Dictionary<string, string> headers = null);
+
+        Task<WebResponse<string>> DeleteAsync(string uri, Dictionary<string, string> headers = null);
     }
 }
--- Frontend/VIAProMa/Assets/i5 Toolkit for Unity/Runtime/Utilities/Rest Connectors/UnityWebRequestRestConnector.cs
+++ Frontend/VIAProMa/Assets/i5 Toolkit for Unity/Runtime/Utilities/Rest Connectors/UnityWebRequestRestConnector.cs
@@ -1,4 +1,5 @@
 ﻿using i5.Toolkit.Core.Utilities.Async;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine.Networking;
 
@@ -6,10 +7,11 @@ namespace i5.Toolkit.Core.Utilities
 {
     public class UnityWebRequestRestConnector : IRestConnector
     {
-        public async Task<WebResponse<string>> DeleteAsync(string uri)
+        public async Task<WebResponse<string>> DeleteAsync(string uri, Dictionary<string, string> headers = null)
         {
             using (UnityWebRequest req = UnityWebRequest.Delete(uri))
             {
+                AddHeaders(req, headers);
                 await req.SendWebRequest();
 
                 if (req.isHttpError || req.isNetworkError)
@@ -23,13 +25,14 @@ namespace i5.Toolkit.Core.Utilities
             }
         }
 
-        public async Task<WebResponse<string>> GetAsync(string uri)
+        public async Task<WebResponse<string>> GetAsync(string uri, Dictionary<string, string> headers = null)
         {
             using (UnityWebRequest req = UnityWebRequest.Get(uri))
             {
+                AddHeaders(req, headers);
                 await req.SendWebRequest();
 
-                if(req.isHttpError || req.isNetworkError)
+                if (req.isHttpError || req.isNetworkError)
                 {
                     return new WebResponse<string>(false, req.downloadHandler.text, req.downloadHandler.data, req.responseCode, req.error);
                 }
@@ -40,10 +43,11 @@ namespace i5.Toolkit.Core.Utilities
             }
         }
 
-        public async Task<WebResponse<string>> PostAsync(string uri, string postData)
+        public async Task<WebResponse<string>> PostAsync(string uri, string postData, Dictionary<string, string> headers = null)
         {
             using (UnityWebRequest req = UnityWebRequest.Post(uri, postData))
             {
+                AddHeaders(req, headers);
                 await req.SendWebRequest();
 
                 if (req.isHttpError || req.isNetworkError)
@@ -57,10 +61,15 @@ namespace i5.Toolkit.Core.Utilities
             }
         }
 
-        public async Task<WebResponse<string>> PutAsync(string uri, string postData)
+        public async Task<WebResponse<string>> PostAsync(string uri, byte[] postData, Dictionary<string, string> headers = null)
         {
-            using (UnityWebRequest req = UnityWebRequest.Put(uri, postData))
+            using (UnityWebRequest req = new UnityWebRequest(uri, "POST"))
             {
+                req.uploadHandler = new UploadHandlerRaw(postData);
+                req.downloadHandler = new DownloadHandlerBuffer();
+
+                AddHeaders(req, headers);
+
                 await req.SendWebRequest();
 
                 if (req.isHttpError || req.isNetworkError)
@@ -71,6 +80,36 @@ namespace i5.Toolkit.Core.Utilities
                 {
                     return new WebResponse<string>(req.downloadHandler.text, req.downloadHandler.data, req.responseCode);
                 }
+            }
+        }
+
+        public async Task<WebResponse<string>> PutAsync(string uri, string postData, Dictionary<string, string> headers = null)
+        {
+            using (UnityWebRequest req = UnityWebRequest.Put(uri, postData))
+            {
+                AddHeaders(req, headers);
+                await req.SendWebRequest();
+
+                if (req.isHttpError || req.isNetworkError)
+                {
+                    return new WebResponse<string>(false, req.downloadHandler.text, req.downloadHandler.data, req.responseCode, req.error);
+                }
+                else
+                {
+                    return new WebResponse<string>(req.downloadHandler.text, req.downloadHandler.data, req.responseCode);
+                }
+            }
+        }
+
+        private void AddHeaders(UnityWebRequest req, Dictionary<string, string> headers)
+        {
+            if (headers == null)
+            {
+                return;
+            }
+            foreach (KeyValuePair<string, string> header in headers)
+            {
+                req.SetRequestHeader(header.Key, header.Value);
             }
         }
     }
