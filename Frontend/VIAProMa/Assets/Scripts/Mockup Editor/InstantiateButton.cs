812cf415ee13892593de2a23a80901ec0a0cb2cd
Lepi009 <leander.pieters@web.de>
2021-06-22 15:25:54 +0200

    Implemented List, Instantiation Funktion

---
+++ Frontend/VIAProMa/Assets/Scripts/Mockup Editor/InstantiateButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class InstantiateButton : MonoBehaviour
+{
+    [SerializeField] MockupEditorList list;
+    [SerializeField] int index;
+    [SerializeField] ButtonConfigHelper buttonConfig;
+
+    MockupEditorItem item;
+
+    private void Start()
+    {
+        item = list.items[index];
+        buttonConfig.MainLabelText = item.name;
+    }
+
+    public void OnClickButton()
+    {
+        Debug.Log("Instantiate Prefab");
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        GameObject baseGO = Instantiate(list.PrefabBase, gameObject.transform.position, Quaternion.identity);
+        GameObject skinGO = Instantiate(item.Prefab, baseGO.transform);
+
+        Interactable interactable;
+        if (skinGO.TryGetComponent<Interactable>(out interactable)) interactable.enabled = false;
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/Mockup Editor/MockupEditorList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = " Mockup Editor/List")]
+public class MockupEditorList : ScriptableObject
+{
+    public GameObject PrefabBase;
+    public List<MockupEditorItem> items;
+}
