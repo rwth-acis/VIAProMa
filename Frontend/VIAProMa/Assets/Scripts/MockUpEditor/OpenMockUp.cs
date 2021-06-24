3d8710aa78033c35f6d0e8c2473dc8e22b7e3d38
Cihan <cihan.orhan@rwth-aachen.de>
2021-06-24 14:15:17 +0200

    Window Prefab created
    Prefab created

---
+++ Frontend/VIAProMa/Assets/Scripts/MockUpEditor/OpenMockUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class OpenMockUp : MonoBehaviourPunCallbacks
+{
+    private AssetBundle myLoadedAssetBundle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Tests/MockUpEditor");
+    }
+
+    public void openNewScene() 
+    {
+        SceneManager.LoadScene("MockUpTest", LoadSceneMode.Single);
+    }
+}
