43d9c697196755b88e2cf92b1483828d1a5731db
Lepi009 <leander.pieters@web.de>
2021-07-10 15:05:38 +0200

    Implemented different categories

--- Frontend/VIAProMa/Assets/Scripts/Mockup Editor/MockUpEditorWindow.cs
+++ Frontend/VIAProMa/Assets/Scripts/Mockup Editor/MockUpEditorWindow.cs
@@ -1,8 +1,28 @@
-﻿using System.Collections;
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MockUpEditorWindow : MonoBehaviour
 {
     public Transform spawnPlace;
+    [SerializeField] List<InstantiateButton> buttons;
+    [SerializeField] List<MockupEditorList> categories;
+
+    public void OnCategorySelected(InteractableToggleCollection collection)
+    {
+        MockupEditorList currentList = categories[collection.CurrentIndex];
+        for(int i = 0; i < buttons.Count; i++)
+        {
+            if(i < currentList.items.Count)
+            {
+                buttons[i].gameObject.SetActive(true);
+                buttons[i].list = currentList;
+                buttons[i].UpdateButton();
+            } else
+            {
+                buttons[i].gameObject.SetActive(false);
+            }
+        }
+    }
 }
