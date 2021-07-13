c7c8a477a445723b015ac7b7cf3715fcf6267c6f
Dan Vonk <dan@danvonk.com>
2021-07-13 15:50:19 +0200

    Add Serializer/Synchronizer stubs

---
+++ Frontend/VIAProMa/Assets/Scripts/Multiplayer/Synchronizer/MinimapSynchronizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using i5.VIAProMa.Multiplayer.Common;
+using Photon.Pun;
+
+namespace i5.VIAProMa.Multiplayer.Synchronizer
+{
+    class MinimapSynchronizer : TransformSynchronizer
+    {
+        private void Awake()
+        {
+
+        }
+
+        public override void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            base.OnPhotonSerializeView(stream, info);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
---
+++ Frontend/VIAProMa/Assets/Scripts/SaveLoadSystem/Serializers/MinimapSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using i5.VIAProMa.SaveLoadSystem.Core;
+using i5.VIAProMa.Visualizations.Minimap;
+using UnityEngine;
+
+namespace i5.VIAProMa.SaveLoadSystem.Serializers
+{
+    class MinimapSerializer : MonoBehaviour, ISerializable
+    {
+        private const string objsKey = "minimap_objs";
+        private MinimapController controller;
+
+        private void Awake()
+        {
+            controller = GetComponent<MinimapController>();
+        }
+
+        public SerializedObject Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize(SerializedObject serializedObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
