7be4b2484a0c80fd5211d9d8c666bd055ae78998
Konstantin Kühlem <konstantin@koeln.de>
2021-12-21 12:51:39 +0100

    Implemented basic class structure for Guided Tour, its Tasks and Sections to give other members reference for their tasks

---
+++ Frontend/VIAProMa/Assets/GuidedTour/Scripts/TourTasks/AbstractTourTask.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GuidedTour
+{
+    /**
+     * <summary>
+     * AbstractTourTask is the abstraction for all specific TourTaks. Every TourTasks has a name, a description 
+     * and a state (active or inactive).
+     * To Do:
+     *  - Every Tour Task should have a reference to its graphical representation and vice versa
+     * </summary>
+     */
+    public abstract class AbstractTourTask : MonoBehaviour
+    {
+        [SerializeField] public string Name { get; internal set; }
+        [SerializeField] public string Description { get; internal set; }
+        [SerializeField] public string Active { get; internal set; }
+
+        internal AbstractTourTask(string name, string description)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+
+        void Start()
+        {
+
+        }
+
+        /**
+         * <summary>
+         * This method is called on every frame by the TourManger whilst this task is active. As soon as this method returns true, 
+         * this task is over and the TourManager selects the next tasks.
+         * </summary>
+         */ 
+        internal abstract bool IsTaskDone();
+    }
+}
---
+++ Frontend/VIAProMa/Assets/GuidedTour/Scripts/TourTasks/SimpleTourTask.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GuidedTour
+{
+    /**
+     * <summary>
+     * SimpleTourTasks repents a TourTasks which contains a name, a description and has a simple "continue" button 
+     * to advance to the next task.
+     * </summary>
+     */
+    public class SimpleTourTask : AbstractTourTask
+    {
+        internal SimpleTourTask(string name, string description) : base(name, description)
+        {
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        internal override bool IsTaskDone()
+        {
+            return true; // To Do: Listen for "continue" button
+        }
+    }
+}
