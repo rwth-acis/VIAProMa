91f7f3f22abcbf9dfab2ad1b2469d54cda9a397c
Felix755 <93538617+Felix755@users.noreply.github.com>
2021-12-28 13:36:13 +0100

    Added DeepLinks Scripts Folder and basic classes
    Added the DeepLinks and InviteLinks folder to the scripts, generated basic code skeletons for the DeepLinkManager and the InviteLinkGenerator

---
+++ Frontend/VIAProMa/Assets/Scripts/DeepLinks/DeepLinkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+namespace i5.VIAProMa.DeepLinks
+{
+    /// <summary>
+    /// Deep Link Manager which handles the reception of Deep Links at runtime
+    /// </summary>
+    public class DeepLinkManager : Singleton<DeepLinkManager>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Frontend/VIAProMa/Assets/Scripts/DeepLinks/InviteLinks/InviteLinkGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace i5.VIAProMa.DeepLinks.InviteLinks
+{
+    public class InviteLinkGenerator : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
