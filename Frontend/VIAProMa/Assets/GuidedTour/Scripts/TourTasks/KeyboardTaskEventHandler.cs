b22508f322410b543b38d2eaf5b283336a2b76df
luk fuchs <luk.fuchs@rwth-aachen.de>
2021-12-29 16:26:56 +0100

    Closes #294 and #295 is still work-in-progress

---
+++ Frontend/VIAProMa/Assets/GuidedTour/Scripts/TourTasks/KeyboardTaskEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+
+namespace GuidedTour
+{
+    [UnityEngine.AddComponentMenu("Scripts/MRTK/SDK/InteractableReceiver")]
+    public class KeyboardTaskEventHandler : ReceiverBaseMonoBehavior
+    {
+
+        public UnityEvent m_MyEvent;
+
+        public override void OnClick(InteractableStates state, Interactable source, IMixedRealityPointer pointer = default) 
+        {
+            NotifyTask();
+        }
+
+        public void NotifyTask() 
+        {
+            Debug.Log("The Keyboard done has been pressed by Luk");
+            m_MyEvent.Invoke();
+        }
+
+    }
+}
---
+++ Frontend/VIAProMa/Assets/GuidedTour/Scripts/TourTasks/KeyboardTourTask.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+namespace GuidedTour {
+    public class KeyboardTourTask : AbstractTourTask
+    {
+        private bool done = false;
+        [SerializeField] private string targetString = "helloWorld.url";
+        [SerializeField] private GameObject textArea;
+        private TextMeshPro input;
+        public Interactable interactable;
+
+        private void Awake() 
+        {
+            input = textArea.GetComponent<TextMeshPro>();
+        }
+
+        internal override void SkipTask() 
+        {
+            if(Active && !done) {
+                input.SetText(targetString);
+                interactable.TriggerOnClick();
+                done = true;
+            }
+        }
+
+        public void temp() 
+        {
+            if (Active && !done) 
+            {
+                input.SetText(targetString);
+                interactable.TriggerOnClick();
+                done = true;
+            }
+        }
+
+        internal override bool IsTaskDone() 
+        {
+            return done;
+        }
+        public void OnAction() 
+        {
+            if (Active && !done) 
+            {
+                if (input.text.Equals(targetString)) 
+                {
+                    Debug.Log("String is matching");
+                    done = true;
+                }
+                else 
+                {
+                    Debug.Log("String is not matching");
+                }
+            }
+        }
+    }
+}
