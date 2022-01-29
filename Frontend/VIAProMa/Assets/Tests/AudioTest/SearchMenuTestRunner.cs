855057548728ecfac3bdcfcc21d40335e04589db
Kowl1307 <Kilian.Sinke@gmx.de>
2022-01-29 17:58:21 +0100

    Added Search functionality for issues and visualizations. Autocmplete with the keyboard

---
+++ Frontend/VIAProMa/Assets/Tests/AudioTest/SearchMenuTestRunner.cs
@@ -0,0 +1,19 @@
+﻿using i5.VIAProMa.UI.SearchMenu;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchMenuTestRunner : MonoBehaviour
+{
+
+    public SearchMenu menu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            menu.gameObject.SetActive(true);
+        }
+
+    }
+}
