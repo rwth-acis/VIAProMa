0b8a50a08a94bc45a6b38f314b04c50048afd9b3
Hana Masara <hana.masara@rwth-aachen.de>
2022-01-30 11:56:55 +0100

    added config Json file

---
+++ Frontend/VIAProMa/Assets/Scripts/DeepLinks/Configuration/LoadData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace ConfigLoadData
+{
+
+    public class LoadData : MonoBehaviour
+    {
+        /// <summary>
+        /// Method to laod Data from JSON Config file
+        /// <returns>an object which contains config data as variables.</returns>
+        /// </summary>
+        public static Config LoadConfig()
+        {
+            string configFilePath = System.IO.Path.Combine(Application.streamingAssetsPath, "config.json");
+            string configData = System.IO.File.ReadAllText(configFilePath);
+            Config config = JsonUtility.FromJson<Config>(configData);
+            return config;
+        }
+
+        [System.Serializable]
+        public class Config
+        {
+            public string scheme;
+            public string invitePath;
+            public string paramName_roomName;
+        }
+    }
+}
