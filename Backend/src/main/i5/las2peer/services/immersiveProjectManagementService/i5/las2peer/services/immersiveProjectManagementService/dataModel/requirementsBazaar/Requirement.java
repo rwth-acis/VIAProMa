0ff0249dd1d54986bbb405550e7faf6d01a71a00
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2022-02-08 11:31:41 +0100

    fixed unrecognized JSON fields in backend

--- Backend/src/main/i5/las2peer/services/immersiveProjectManagementService/i5/las2peer/services/immersiveProjectManagementService/dataModel/requirementsBazaar/Requirement.java
+++ Backend/src/main/i5/las2peer/services/immersiveProjectManagementService/i5/las2peer/services/immersiveProjectManagementService/dataModel/requirementsBazaar/Requirement.java
@@ -1,8 +1,11 @@
 package i5.las2peer.services.immersiveProjectManagementService.i5.las2peer.services.immersiveProjectManagementService.dataModel.requirementsBazaar;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by bened on 09.05.2019.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Requirement {
     private int id;
     private String name;
