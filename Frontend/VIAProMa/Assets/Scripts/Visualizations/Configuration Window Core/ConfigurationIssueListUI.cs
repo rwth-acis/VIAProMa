6882c7bd13e321125d3977d64d519f68bf1e809b
Simonsator <00pflaume@gmail.com>
2022-06-16 19:45:18 +0200

    Added a new button to configuration window

---
+++ Frontend/VIAProMa/Assets/Scripts/Visualizations/Configuration Window Core/ConfigurationIssueListUI.cs
@@ -0,0 +1,60 @@
+﻿using i5.VIAProMa.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+namespace i5.VIAProMa.Visualizations.ColorConfigWindow
+{
+    public class ConfigurationIssueListUI : MonoBehaviour, IUiFragment
+    {
+        [SerializeField] private Interactable selectionButton;
+        [SerializeField] private GameObject selectionActiveMessage;
+
+        private bool uiEnabled = true;
+        private Visualization visualization;
+
+        public bool UIEnabled
+        {
+            get => uiEnabled;
+            set
+            {
+                uiEnabled = value;
+                selectionButton.Enabled = uiEnabled;
+            }
+        }
+
+        private void Awake()
+        {
+            if (selectionButton == null)
+            {
+                SpecialDebugMessages.LogMissingReferenceError(this, nameof(selectionButton));
+            }
+            if (selectionActiveMessage == null)
+            {
+                SpecialDebugMessages.LogMissingReferenceError(this, nameof(selectionActiveMessage));
+            }
+            else
+            {
+                selectionActiveMessage.SetActive(false);
+            }
+        }
+
+        public void Setup(Visualization visualization)
+        {
+            this.visualization = visualization;
+        }
+
+        public void SelectIssues()
+        {
+            visualization.ContentProvider.SelectContent();
+            UIEnabled = false;
+            selectionActiveMessage.SetActive(true);
+        }
+
+        public void EndIssueSelection()
+        {
+            visualization.ContentProvider.EndContentSelection();
+            UIEnabled = true;
+            selectionActiveMessage.SetActive(false);
+        }
+    }
+}
