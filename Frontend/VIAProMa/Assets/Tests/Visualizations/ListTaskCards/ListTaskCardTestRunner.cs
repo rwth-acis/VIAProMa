fa43282d77d5239bf9e1dfe4c6af21b1514fb24a
Simonsator <00pflaume@gmail.com>
2022-06-19 13:44:38 +0200

    Added test runner to list task card test

---
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/ListTaskCards/ListTaskCardTestRunner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using i5.VIAProMa.DataModel.API;
+using i5.VIAProMa.Visualizations;
+using i5.VIAProMa.Visualizations.Competence;
+using UnityEngine;
+
+public class ListTaskCardTestRunner : MonoBehaviour
+{
+    [Range(1, 10)] public int[] simulatedIssues;
+    public Visualization visualizations;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            for (int i = 0; i < simulatedIssues.Length; i++)
+            {
+                var issue = new Issue(DataSource.GITHUB, -1, "Test", "A test", 0, null, IssueStatus.CLOSED, "", "", null, null);
+                visualizations.ContentProvider.Issues.Add(issue);
+            }
+        }
+    }
+}
