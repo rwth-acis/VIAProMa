44c7ce199cf0236fb8eafd06556beca789346d06
Simonsator <00pflaume@gmail.com>
2022-06-27 17:44:01 +0200

    More consistent code formatting

--- Frontend/VIAProMa/Assets/Tests/Visualizations/ListTaskCards/ListTaskCardTestRunner.cs
+++ Frontend/VIAProMa/Assets/Tests/Visualizations/ListTaskCards/ListTaskCardTestRunner.cs
@@ -1,28 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using i5.VIAProMa.DataModel.API;
 using i5.VIAProMa.Visualizations;
-using i5.VIAProMa.Visualizations.Competence;
 using UnityEngine;
 
-public class ListTaskCardTestRunner : MonoBehaviour
+namespace Tests.Visualizations.ListTaskCards
 {
-    [Range(1, 10)] public int[] simulatedIssues;
-    public Visualization visualizations;
-
-    void Update()
+    public class ListTaskCardTestRunner : MonoBehaviour
     {
-        if (!Input.GetKeyDown(KeyCode.F5)) return;
-        visualizations.ContentProvider.SelectContent();
-        
-        for (var i = 0; i < simulatedIssues.Length; i++)
-        {
-            var issue = new Issue(DataSource.GITHUB, -1, "Test", "A test", 0, null, IssueStatus.CLOSED, "", "",
-                null, null);
-            visualizations.ContentProvider.Issues.Add(issue);
-        }
+        [Range(1, 10)] public int[] simulatedIssues;
+        public Visualization visualizations;
 
-        visualizations.ContentProvider.EndContentSelection();
+        private void Update()
+        {
+            if (!Input.GetKeyDown(KeyCode.F5)) return;
+            visualizations.ContentProvider.SelectContent();
+        
+            for (var i = 0; i < simulatedIssues.Length; i++)
+            {
+                var issue = new Issue(DataSource.GITHUB, -1, "Test", "A test", 0, null, IssueStatus.CLOSED, "", "",
+                    null, null);
+                visualizations.ContentProvider.Issues.Add(issue);
+            }
+
+            visualizations.ContentProvider.EndContentSelection();
+        }
     }
 }
