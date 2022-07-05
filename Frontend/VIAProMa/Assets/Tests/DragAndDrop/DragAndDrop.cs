215e59a27a834bc8eed74ae71091320fcd06d9a2
Paul Weiser <paul.weiser@rwth-aachen.de>
2022-07-05 14:22:33 +0200

    Destruction of Issue card after addition to visualization implemented
    The Issue card is destroyed with a fancy animation which shows to
    which visualization it is added.
    
    currently only one of the potentially multiple visualizations is
    shown as the target. For more visualizations, the issue card would
    have to be duplicated.

--- Frontend/VIAProMa/Assets/Tests/DragAndDrop/DragAndDrop.cs
+++ Frontend/VIAProMa/Assets/Tests/DragAndDrop/DragAndDrop.cs
@@ -3,6 +3,7 @@ using i5.VIAProMa.IssueSelection;
 using i5.VIAProMa.UI.ListView.Issues;
 using i5.VIAProMa.Utilities;
 using i5.VIAProMa.Visualizations;
+using Photon.Pun;
 
 using Microsoft.MixedReality.Toolkit.UI;
 
@@ -23,9 +24,19 @@ public class DragAndDrop : MonoBehaviour
     IssueSelector IssueManipulator;
     IssueDataDisplay issueDataDisplay;
     ObjectManipulator grabComponent;
+    GameObject issueGameObject;
 
     //A list of Visualizations that currently overlap with the Issue
     List<GameObject> currentHits;
+
+    //indicates that the Issue is being destroyed right now; happens after it is added to a visualization
+    bool IssueIsDestroyed = false;
+    [SerializeField]
+    [Tooltip("Time the component gets before it is destroyed after being added to a visualization")]
+    float destroyTime = 0.3f;
+    float timeOffset = 0;
+    Vector3 destroyStartPosition;
+    Vector3 destroyStartSize;
 
     //Awake is called when the script instance is being loaded
     void Awake()
@@ -37,6 +48,8 @@ public class DragAndDrop : MonoBehaviour
         {
             SpecialDebugMessages.LogComponentNotFoundError(this, nameof(IssueSelector), gameObject);
         }
+
+        issueGameObject = IssueManipulator.gameObject;
 
         issueDataDisplay = GetComponentInParent<IssueDataDisplay>();
         if(issueDataDisplay == null)
@@ -54,6 +67,40 @@ public class DragAndDrop : MonoBehaviour
         //Make it so object is not influenced by forces
         GetComponent<Rigidbody>().isKinematic = true;
         GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    private void Update()
+    {
+        //if the issue is set to be destroyed, move it towards the visualization it is added to
+        if(IssueIsDestroyed)
+        {
+            //if visualizations were moved out of reach of the Issue it doesn't have a target to move to anymore
+            if(currentHits.Count == 0)
+            {
+                Debug.LogWarning("Issue is in destroy routine, but there is no visualization in its reach anymore. " +
+                    "Destroy it immediately.");
+                //destroy component over the network. If that doesn't work, destroy it locally
+                try{PhotonNetwork.Destroy(issueGameObject);}
+                catch{Destroy(issueGameObject);}
+            }
+
+            timeOffset += Time.deltaTime * (1.0f / destroyTime);
+
+            //move Issue towards visualization, such that it reaches it after destroyTime seconds
+            Vector3 endPosition = currentHits[0].transform.position;
+            issueGameObject.transform.position = Vector3.Lerp(destroyStartPosition, endPosition, timeOffset);
+
+            //change the size so it shrinks while moving
+            issueGameObject.transform.localScale = Vector3.Lerp(destroyStartSize, destroyStartSize * 0.1f, timeOffset);
+
+            //destroy the Issue when it reaches its goal
+            if (timeOffset >= 1)
+            {
+                //destroy component over the network. If that doesn't work, destroy it locally
+                try { PhotonNetwork.Destroy(issueGameObject); }
+                catch { Destroy(issueGameObject); }
+            }
+        }
     }
 
     #region TriggerEvents
@@ -88,11 +135,15 @@ public class DragAndDrop : MonoBehaviour
         issueList.Add(issueDataDisplay.Content);
         visualization.ContentProvider.Issues = issueList;
 
-        Debug.Log("Issue " + issueDataDisplay.Content.Name + " added to " + visualization.gameObject.name);//TODO remove
+        //set the start position and size of the destroy routine in the update function
+        destroyStartPosition = issueGameObject.transform.position;
+        destroyStartSize = issueGameObject.transform.localScale;
+        IssueIsDestroyed = true;
     }
 
     void RemoveObjectFromHitsList(GameObject target)
     {
+        //test if target is a visualization
         Visualization visualization = target.GetComponentInParent<Visualization>();
         if (visualization == null)
         {
@@ -125,7 +176,6 @@ public class DragAndDrop : MonoBehaviour
         }
 
         currentHits.Add(visualization.gameObject);
-        //Debug.Log(transform.parent.name + ": " + visualization.gameObject.name + " added to the Hits list.");
 
         //Activate selection indicator of the issue
         IssueManipulator.Selected = true;
