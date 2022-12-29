dd99f7b4ad0b6db6915d072f1dda4dc754d579d7
Tschlompf <96498827+Tschlompf@users.noreply.github.com>
2022-12-29 18:56:40 +0100

    Imported Objects get resized

--- Frontend/VIAProMa/Assets/Scripts/3DImport/FileDownloader.cs
+++ Frontend/VIAProMa/Assets/Scripts/3DImport/FileDownloader.cs
@@ -3,23 +3,48 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using Siccity.GLTFUtility;
+using static System.Net.WebRequestMethods;
+using System.Text;
 
 public class FileDownloader : MonoBehaviour {
-    //public GameObject testModel; 
+    [SerializeField] private GameObject boundingBox;
+    private string webLink;
+    private string fileName;
 
     void Start () {
+        webLink = "http://www.nikita-zaloga.de/undertale-_low_poly.glb";
+        fileName = "undertale - _low_poly.glb";
         StartCoroutine(DownloadFile());
     }
 
     IEnumerator DownloadFile() {
-        var uwr = new UnityWebRequest("http://www.nikita-zaloga.de/undertale-_low_poly.glb", UnityWebRequest.kHttpVerbGET);
-        string path = Path.Combine(Application.persistentDataPath, "undertale-_low_poly.glb");
+        var uwr = new UnityWebRequest(webLink, UnityWebRequest.kHttpVerbGET);
+        string path = Path.Combine(Application.persistentDataPath, fileName);
         uwr.downloadHandler = new DownloadHandlerFile(path);
         yield return uwr.SendWebRequest();
         if (uwr.result != UnityWebRequest.Result.Success)
+        {
             Debug.LogError(uwr.error);
+        }
         else
+        {
             Debug.Log("File successfully downloaded and saved to " + path);
+            
+            //import into unity scene
             GameObject testModel = Importer.LoadFromFile(path);
+            
+            //GameObject myBoundingBox = Instantiate(boundingBox);
+            //myBoundingBox.transform.parent = testModel.transform;
+
+            MeshFilter[] rr = testModel.GetComponentsInChildren<MeshFilter>();
+            Bounds bounds = rr[0].mesh.bounds;
+            foreach (MeshFilter r in rr) { bounds.Encapsulate(r.mesh.bounds); }
+
+
+            //myBoundingBox.GetComponent<BoxCollider>().size = bounds.size;
+
+            testModel.transform.localScale = testModel.transform.localScale / bounds.size.magnitude;
+
+        }
     }
 }
