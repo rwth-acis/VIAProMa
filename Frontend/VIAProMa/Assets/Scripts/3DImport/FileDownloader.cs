e1b3c375491469c04be992d7c00e46b5aa8d7fdb
Tschlompf <96498827+Tschlompf@users.noreply.github.com>
2022-12-30 00:58:29 +0100

    Fixed FileDownloader Script

--- Frontend/VIAProMa/Assets/Scripts/3DImport/FileDownloader.cs
+++ Frontend/VIAProMa/Assets/Scripts/3DImport/FileDownloader.cs
@@ -5,46 +5,76 @@ using UnityEngine.Networking;
 using Siccity.GLTFUtility;
 using static System.Net.WebRequestMethods;
 using System.Text;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Diagnostics;
+using System;
 
 public class FileDownloader : MonoBehaviour {
-    [SerializeField] private GameObject boundingBox;
-    private string webLink;
-    private string fileName;
+    public GameObject wrapper;
+    
+    //private string webLink;
+    //private string fileName;
 
     void Start () {
-        webLink = "http://www.nikita-zaloga.de/undertale-_low_poly.glb";
-        fileName = "undertale - _low_poly.glb";
-        StartCoroutine(DownloadFile());
-    }
+        string webLink = "http://www.nikita-zaloga.de/undertale-_low_poly.glb";
+        string fileName = System.IO.Path.GetFileName(webLink);
+        string folderName = "3Dobjects";
 
-    IEnumerator DownloadFile() {
-        var uwr = new UnityWebRequest(webLink, UnityWebRequest.kHttpVerbGET);
-        string path = Path.Combine(Application.persistentDataPath, fileName);
-        uwr.downloadHandler = new DownloadHandlerFile(path);
-        yield return uwr.SendWebRequest();
-        if (uwr.result != UnityWebRequest.Result.Success)
+        string path = Path.Combine(Application.persistentDataPath, folderName, fileName);
+
+        if (!System.IO.File.Exists(path))
         {
-            Debug.LogError(uwr.error);
+            StartCoroutine(DownloadFile(path, webLink));
         }
         else
         {
-            Debug.Log("File successfully downloaded and saved to " + path);
-            
-            //import into unity scene
-            GameObject testModel = Importer.LoadFromFile(path);
-            
-            //GameObject myBoundingBox = Instantiate(boundingBox);
-            //myBoundingBox.transform.parent = testModel.transform;
-
-            MeshFilter[] rr = testModel.GetComponentsInChildren<MeshFilter>();
-            Bounds bounds = rr[0].mesh.bounds;
-            foreach (MeshFilter r in rr) { bounds.Encapsulate(r.mesh.bounds); }
-
-
-            //myBoundingBox.GetComponent<BoxCollider>().size = bounds.size;
-
-            testModel.transform.localScale = testModel.transform.localScale / bounds.size.magnitude;
-
+            UnityEngine.Debug.Log("File already saved in " + path + ", not downloading");
+            LoadModel(path);
         }
+
+    }
+
+    void LoadModel(String path)
+    {
+        //import into unity scene
+        GameObject testModel = Importer.LoadFromFile(path);
+        testModel.transform.SetParent(wrapper.transform);
+
+        //resize object according to mesh bounds
+        MeshFilter[] rr = testModel.GetComponentsInChildren<MeshFilter>();
+        Bounds bounds = rr[0].mesh.bounds;
+        foreach (MeshFilter r in rr) { bounds.Encapsulate(r.mesh.bounds); }
+
+        testModel.transform.localScale = testModel.transform.localScale / bounds.size.magnitude;
+
+        //add interactables and collider
+        testModel.AddComponent<BoxCollider>();
+        testModel.GetComponent<BoxCollider>().size = bounds.size;
+        testModel.GetComponent<BoxCollider>().center = bounds.center;
+        testModel.AddComponent<NearInteractionGrabbable>();
+        testModel.AddComponent<ObjectManipulator>();
+        testModel.GetComponent<ObjectManipulator>().HostTransform = testModel.transform;
+    }
+    IEnumerator DownloadFile(string path, string webLink) {
+        //downloads file
+        var uwr = new UnityWebRequest(webLink, UnityWebRequest.kHttpVerbGET);
+        uwr.downloadHandler = new DownloadHandlerFile(path);
+        yield return uwr.SendWebRequest();
+        while (!uwr.downloadHandler.isDone)
+        {
+            yield return null;
+        }
+
+        if (uwr.result != UnityWebRequest.Result.Success)
+        {
+            UnityEngine.Debug.LogError(uwr.error);
+        }
+        
+        uwr.downloadHandler.Dispose();
+        UnityEngine.Debug.Log("File successfully downloaded and saved to " + path);
+
+        LoadModel(path);
+        yield return null;
     }
 }
