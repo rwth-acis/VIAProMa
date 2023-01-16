f5d252dab106de3335b4805fdee98000c20a3195
Mika Specht <mika.specht@rwth-aachen.de>
2023-01-16 17:48:18 +0100

    Added missing classes

---
+++ Frontend/VIAProMa/Assets/Scripts/Analytics/AnalyticsSettings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class AnalyticsSettings
+{
+    public bool AnalyticsEnabled {set; get;}
+}
