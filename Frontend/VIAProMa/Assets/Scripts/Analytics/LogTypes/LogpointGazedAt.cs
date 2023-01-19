02a1338c5c22718d0ed49ee89d1166273858eb90
Lennart S <55597910+LennartCode@users.noreply.github.com>
2023-01-19 23:58:46 +0100

    Renamed fields for clarification

--- Frontend/VIAProMa/Assets/Scripts/Analytics/LogTypes/LogpointGazedAt.cs
+++ Frontend/VIAProMa/Assets/Scripts/Analytics/LogTypes/LogpointGazedAt.cs
@@ -2,20 +2,20 @@ namespace VIAProMa.Assets.Scripts.Analytics.LogTypes
 {
     public class LogpointGazedAt : Logpoint
     {
-        public string ProjectName { get; set; }
-        public string LogID { get; set; }
+        public string VIAProMaProjectID { get; set; }
+        public string LoggedObjectID { get; set; }
         public string LoggedObjectType { get; set; }
 
-        public LogpointGazedAt(string ProjectName, string LogID, string LoggedObjectType)
+        public LogpointGazedAt(string VIAProMaProjectID, string LoggedObjectID, string LoggedObjectType)
         {
-            this.ProjectName = ProjectName;
-            this.LogID = LogID;
+            this.VIAProMaProjectID = VIAProMaProjectID;
+            this.LoggedObjectID = LoggedObjectID;
             this.LoggedObjectType = LoggedObjectType;
         }
 
         public override string ToString()
         {
-            return string.Format("{0}: Project Name: {1}, LogID: {2} LoggedType: {3}", base.Timestamp, ProjectName, LogID, LoggedObjectType);
+            return string.Format("{0}: VIAProMaProjectID: {1}, LoggedObjectID: {2} LoggedType: {3}", base.Timestamp, VIAProMaProjectID, LoggedObjectID, LoggedObjectType);
         }
     }
 }
