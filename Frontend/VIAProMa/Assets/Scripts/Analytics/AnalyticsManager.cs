015b095065713854ab216b240f06c0149ba2a899
Pascal Raabe <pascalraabe.buis@gmail.com>
2023-01-22 00:35:29 +0100

    Implemented Notification Popup with Sound on Telemetry enabled toggled

--- Frontend/VIAProMa/Assets/Scripts/Analytics/AnalyticsManager.cs
+++ Frontend/VIAProMa/Assets/Scripts/Analytics/AnalyticsManager.cs
@@ -4,33 +4,74 @@ using Microsoft.MixedReality.Toolkit.Utilities;
 using i5.VIAProMa.WebConnection;
 using UnityEngine;
 using Newtonsoft.Json;
+using Microsoft.MixedReality.Toolkit;
+using System;
+using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class AnalyticsManager : Singleton<AnalyticsManager>
 {
+    // Everything concerned with the Notification PopUp
     private AnalyticsSettings _settings;
+    public Text TextObject;
+    public GameObject Background;
+    public DateTime ExpiresAt;
+    private bool isStartOver = false;
+    public AudioSource NotificationSound;
 
     [SerializeField]
-    public bool AnalyticsEnabled {
-        get { return _settings.AnalyticsEnabled; } 
-        set {
-            PhotonView.Get(this).RPC("SetAnalyticsEnabled", RpcTarget.Others, value);
+    public bool AnalyticsEnabled
+    {
+        get { return _settings.AnalyticsEnabled; }
+        set
+        {
+            // Let other players pull the new settings.
+            PhotonView.Get(this).RPC("SetIsAnalyticsEnabled", RpcTarget.Others, value);
             _settings.AnalyticsEnabled = value;
+
+            ShowIsTelemetryEnabledPopup();
+            // Let other players display the new telemetry enabled state popup.
+            PhotonView.Get(this).RPC("ShowIsTelemetryEnabledPopup", RpcTarget.Others);
+
             SetSettingsOnBackend();
         }
     }
 
+    public void Update()
+    {
+        bool showPopup = (DateTime.Now < ExpiresAt) && isStartOver;
+        Background.SetActive(showPopup);
+        TextObject.enabled = showPopup;
+    }
+
+    [PunRPC]
+    public void ShowIsTelemetryEnabledPopup()
+    {
+        // TODO: Remove the boolean parameter alibi. In is not used anywhere but Photon complains if there isn't a boolean parameter here. We have no clue why, but it works this way.
+        TextObject.text = AnalyticsManager.Instance.AnalyticsEnabled ? "Telemetry Enabled!" : "Telemetry Disabled!";
+        ExpiresAt = DateTime.Now.AddSeconds(2.5);
+        NotificationSound.Play();
+    }
 
     public AnalyticsManager()
     {
-         _settings = new AnalyticsSettings();
+        _settings = new AnalyticsSettings();
     }
 
-    public void Start()
+    public async Task Start()
     {
-        GetSettingsFromBackendAsync();
+        await GetSettingsFromBackendAsync();
+        if (_settings.AnalyticsEnabled && !CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingEnabled)
+        {
+            Debug.LogError("Eye tracking is disabled! Instuctions: " +
+                "https://learn.microsoft.com/en-us/windows/mixed-reality/mrtk-unity/mrtk2/features/input/eye-tracking/eye-tracking-basic-setup?view=mrtkunity-2022-05#testing-your-unity-app-on-a-hololens-2");
+        }
+        TextObject.text = "";
+        ShowIsTelemetryEnabledPopup();
+        isStartOver = true;
     }
 
-    private async void GetSettingsFromBackendAsync()
+    private async Task GetSettingsFromBackendAsync()
     {
         Response resp =
                 await Rest.GetAsync(
@@ -41,7 +82,7 @@ public class AnalyticsManager : Singleton<AnalyticsManager>
                     true);
         ConnectionManager.Instance.CheckStatusCode(resp.ResponseCode);
         string responseBody = await resp.GetResponseBody();
-        _settings = JsonConvert.DeserializeObject<AnalyticsSettings>(responseBody);
+        _settings = JsonConvert.DeserializeObject<AnalyticsSettings>(responseBody); // TODO: Edit to only get Analytics enabled key, when updating to project based analytics.
     }
 
     private async void SetSettingsOnBackend()
@@ -54,7 +95,7 @@ public class AnalyticsManager : Singleton<AnalyticsManager>
     }
 
     [PunRPC]
-    private void SetAnalyticsEnabled(bool enabled)
+    private void SetIsAnalyticsEnabled(bool enabled)
     {
         _settings.AnalyticsEnabled = enabled;
     }
---
+++ Frontend/VIAProMa/Assets/Scripts/Analytics/GazableComponents/GazableIssueCard.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using VIAProMa.Assets.Scripts.Analytics.LogTypes;
+using VIAProMa.Assets.Scripts.Analytics;
+using i5.VIAProMa.UI.ListView.Issues;
+
+public class GazableIssueCard : MonoBehaviour, IObservable<LogpointGazedAt>
+{
+    private List<IObserver<LogpointGazedAt>> observers = new List<IObserver<LogpointGazedAt>>();
+
+    void Awake()
+    {
+        _ = new IssueCardGazingObserver(this);
+    }
+    public void GazeDetected()
+    {
+        string id = this.GetComponent<IssueDataDisplay>().Content.Id.ToString();
+        Debug.Log("ID der Issue" + id);
+        LogpointGazedAt logpoint = new LogpointGazedAt("VIAProMa Project Name", id, "IssueCard");  //TODO: Fill with data!
+        NotifyObservers(logpoint);
+    }
+    // Implementation of necessary methods for the analytics module
+    #region Observer Implemenations
+    public IDisposable Subscribe(IObserver<LogpointGazedAt> observer)
+    {
+        if (!observers.Contains(observer))
+            observers.Add(observer);
+        return new Unsubscriber(observers, observer);
+    }
+
+    public void NotifyObservers(LogpointGazedAt log)
+    {
+        if (AnalyticsManager.Instance.AnalyticsEnabled)
+            foreach (var observer in observers)
+                observer.OnNext(log);
+    }
+
+
+    // This class must be implemented to enable an Observer to unsubscribe from an Observable.
+    // The interface IObservable<...> requires the return type to implement the IDisposable interface which is satisfied by the Unsubscriber class.
+    private class Unsubscriber : IDisposable
+    {
+        private List<IObserver<LogpointGazedAt>> _observers;
+        private IObserver<LogpointGazedAt> _observer;
+
+        public Unsubscriber(List<IObserver<LogpointGazedAt>> observers, IObserver<LogpointGazedAt> observer)
+        {
+            this._observers = observers;
+            this._observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+    class IssueCardGazingObserver : ObserverWrapper<LogpointGazedAt>.Observer
+    {
+        public IssueCardGazingObserver(IObservable<LogpointGazedAt> observable) : base(observable) { }
+    }
+    #endregion Observer Implemenations
+}
