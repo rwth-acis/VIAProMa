4a133b0542beb74061a286a786141428d25f3563
Lennart S <55597910+LennartCode@users.noreply.github.com>
2023-01-25 15:40:19 +0100

    Split Object in LRS schema up into ObjectType and ObjectID.

--- Frontend/VIAProMa/Assets/Scripts/Analytics/LogTypes/LogpointLRSExportable.cs
+++ Frontend/VIAProMa/Assets/Scripts/Analytics/LogTypes/LogpointLRSExportable.cs
@@ -4,18 +4,20 @@ namespace VIAProMa.Assets.Scripts.Analytics.LogTypes
     {
         public string Actor { get; set; }
         public string Verb { get; set; }
-        public string Object { get; set; }
+        public string ObjectType { get; set; }
+        public string ObjectID { get; set; }
 
-        public LogpointLRSExportable(string Actor, string Verb, string Object)
+        public LogpointLRSExportable(string Actor, string Verb, string ObjectType, string ObjectID)
         {
             this.Actor = Actor;
             this.Verb = Verb;
-            this.Object = Object;
+            this.ObjectType = ObjectType;
+            this.ObjectID = ObjectID;
         }
 
         public override string ToString()
         {
-            return base.ToString() + string.Format(", Actor: {0}, Verb: {1}, Object: {2}", Actor, Verb, Object);
+            return base.ToString() + string.Format(", Actor: {0}, Verb: {1}, ObjectType: {2}, ObjectID: {3}", Actor, Verb, ObjectType, ObjectID);
         }
     }
 }
