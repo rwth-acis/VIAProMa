b1c4ac51e5b34ce0b09c7479b7ebad0891e5b9f1
Jona Steffen <69996027+jonast2@users.noreply.github.com>
2023-01-25 10:13:08 +0100

    Fixed AppBarTransformCommand
    Fixed AppBarTransformCommand
    
    The Redo of AppBarTransformCommand was fixed.
    As the endValues were not initialized the AppBar was moved to the origin when activating it. Now the values are initialized upon creation.

--- Frontend/VIAProMa/Assets/Scripts/UndoRedoSystem/AppBarTransformCommand.cs
+++ Frontend/VIAProMa/Assets/Scripts/UndoRedoSystem/AppBarTransformCommand.cs
@@ -11,9 +11,7 @@ public class AppBarTransformCommand : ICommand
     private Vector3 startScale;
     private Vector3 endScale;
 
-    private GameObject currentObject;
     private i5.VIAProMa.UI.AppBar.AppBarPlacer appBarPlacer;
-    // appBarPlacer = currentObject.GetComponent<i5.VIAProMa.UI.AppBar.AppBarPlacer>();
 
     public AppBarTransformCommand(Vector3 pStartPosition, Quaternion pStartRotation, Vector3 pStartScale, i5.VIAProMa.UI.AppBar.AppBarPlacer pAppBarPlacer)
     {
@@ -21,11 +19,14 @@ public class AppBarTransformCommand : ICommand
         startRotation = pStartRotation;
         startScale = pStartScale;
         appBarPlacer = pAppBarPlacer;
+
+        endPosition = appBarPlacer.TargetBoundingBox.Target.transform.localPosition;
+        endRotation = appBarPlacer.TargetBoundingBox.Target.transform.localRotation;
+        endScale = appBarPlacer.TargetBoundingBox.Target.transform.localScale;
     }
 
     public void Execute()
     {
-        //startPosition = appBarPlacer.TargetBoundingBox.Target.transform.localPosition;
         appBarPlacer.TargetBoundingBox.Target.transform.localPosition = endPosition;
         appBarPlacer.TargetBoundingBox.Target.transform.localRotation = endRotation;
         appBarPlacer.TargetBoundingBox.Target.transform.localScale = endScale;
@@ -39,12 +40,5 @@ public class AppBarTransformCommand : ICommand
         appBarPlacer.TargetBoundingBox.Target.transform.localPosition = startPosition;
         appBarPlacer.TargetBoundingBox.Target.transform.localRotation = startRotation;
         appBarPlacer.TargetBoundingBox.Target.transform.localScale = startScale;
-    }
- 
-    public void Redo()
-    {
-        appBarPlacer.TargetBoundingBox.Target.transform.localPosition = endPosition;
-        appBarPlacer.TargetBoundingBox.Target.transform.localRotation = endRotation;
-        appBarPlacer.TargetBoundingBox.Target.transform.localScale = endScale;
     }
 }
