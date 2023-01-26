36af819453cee7cffebd15e38c40a533d1f616b1
Pyxl <113857368+uwuPyxl@users.noreply.github.com>
2023-01-26 19:25:26 +0100

    properly scale ImportedModel collider

--- Frontend/VIAProMa/Assets/Scripts/3DImport/ImportedModel.cs
+++ Frontend/VIAProMa/Assets/Scripts/3DImport/ImportedModel.cs
@@ -27,8 +27,8 @@ public class ImportedModel : MonoBehaviour
 
 		ModelImporter importer = Singleton<ModelImporter>.Instance;
 		GameObject model = importer.InstantiateModel(path);
-        GetComponent<BoxCollider>().size = model.GetComponent<BoxCollider>().size;
-        GetComponent<BoxCollider>().center = model.GetComponent<BoxCollider>().center;
+        GetComponent<BoxCollider>().size = model.GetComponent<BoxCollider>().size * model.transform.localScale.y;
+        GetComponent<BoxCollider>().center = model.GetComponent<BoxCollider>().center * model.transform.localScale.y;
 		model.transform.SetParent(transform);
 		model.transform.localPosition = Vector3.zero;
 
