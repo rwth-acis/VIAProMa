046fda66990a9b33e1e9de6b690733d802b02b40
Tschlompf <96498827+Tschlompf@users.noreply.github.com>
2023-01-27 15:55:53 +0100

    Testing BoundingBox error

--- Frontend/VIAProMa/Assets/Scripts/3DImport/ImportedModel.cs
+++ Frontend/VIAProMa/Assets/Scripts/3DImport/ImportedModel.cs
@@ -31,8 +31,8 @@ public class ImportedModel : MonoBehaviour
 
 		ModelImporter importer = Singleton<ModelImporter>.Instance;
 		GameObject model = importer.InstantiateModel(path);
-        GetComponent<BoxCollider>().size = model.GetComponent<BoxCollider>().size;
-        GetComponent<BoxCollider>().center = model.GetComponent<BoxCollider>().center;
+        //GetComponent<BoxCollider>().size = model.GetComponent<BoxCollider>().size;
+        //GetComponent<BoxCollider>().center = model.GetComponent<BoxCollider>().center;
 		model.transform.SetParent(transform);
 		model.transform.localPosition = Vector3.zero;
         model.transform.localRotation = Quaternion.identity;
