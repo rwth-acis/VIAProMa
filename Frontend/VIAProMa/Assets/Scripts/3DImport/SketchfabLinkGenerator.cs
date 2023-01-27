f610b79b7b095d21646f89a24b6a5300dc15ef1a
Roman Scholz <roman.scholz123@gmail.com>
2023-01-27 19:04:02 +0100

    added new sketchfabItem + download link getter

---
+++ Frontend/VIAProMa/Assets/Scripts/3DImport/SketchfabLinkGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SketchfabLinkGenerator : MonoBehaviour
+{ 
+    private class Result
+    {
+        private string DownloadLink { get; set; } 
+    }
+
+    public string Uid;
+
+    public IEnumerator GetDownloadLink()
+    {
+        string uri = string.Format("https://api.sketchfab.com/v3/model/{0}/download", Uid);
+        //Debug.Log(uri);
+
+        using UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+
+        webRequest.SetRequestHeader("Accept", "application/json");
+        //webRequest.SetRequestHeader("Host", "api.sketchfab.com");
+        webRequest.SetRequestHeader("Authorization", "Token 182f243dcffb4e3e830788ceb1467c33");
+
+        yield return webRequest.SendWebRequest();
+    }
+}
